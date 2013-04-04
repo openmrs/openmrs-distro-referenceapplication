23e11fe08108a05d5c66c7a8fe9d769df8328d45
Chris Briesemeister <breeze@breeze-ubuntu-1204.(none)>
2013-04-04 09:47:03 -0500

    Breeze: RA-29 Adding Omods to exploded war after deployment.

--- packaging/debian-build.sh
+++ packaging/debian-build.sh
@@ -4,14 +4,17 @@ MavenRootDir=$(dirname `pwd`)
 DistroDir=${MavenRootDir}/target/distro
 DebCtrlDir=target/openmrs-reference/DEBIAN
 DebWarDir=target/openmrs-reference/var/lib/tomcat6/webapps/
+OmodTemp=target/openmrs-reference/tmp/openmrs-omods
 
 prepareDir() {
     if [ -d ${DistroDir} ];
         then
-            mkdir -p ${DebCtrlDir}
+            #mkdir -p ${DebCtrlDir}
             mkdir -p ${DebWarDir}
+            mkdir -p ${OmodTemp}
             cp ${DistroDir}/*.war ${DebWarDir}/openmrs.war
-            cp control ${DebCtrlDir}
+            cp ${DistroDir}/*.omod ${OmodTemp}
+            cp -r DEBIAN ${DebCtrlDir}
         else
             echo "Can't find the distribution dir.  Do you need to run mvn package?"
     fi
