cf3078ffdd80268bc95249751e4ba7f169f33a62
Chris Briesemeister <breeze@breeze-ubuntu-1204.(none)>
2013-04-04 14:17:36 -0500

    breeze: RA-29 putting proper info into control file

--- packaging/debian-build.sh
+++ packaging/debian-build.sh
@@ -15,17 +15,28 @@ prepareDir() {
             cp ${DistroDir}/*.war ${DebWarDir}/openmrs.war
             cp ${DistroDir}/*.omod ${OmodTemp}
             cp -r DEBIAN ${DebCtrlDir}
+            applyVersion
         else
             echo "Can't find the distribution dir.  Do you need to run mvn package?"
     fi
 }
 createDeb() {
     cd target
-    dpkg-deb -b openmrs-reference openmrs.deb
+    dpkg-deb -b openmrs-reference openmrs-${version}.deb
     #dpkg-deb --build openmrs-reference
     #equivs-build openmrs-reference
-
 }
+
+applyVersion() {
+    if [ -z ${version} ];
+        then
+            echo "This looks like it is not being run from CI."
+        else
+            sed -i 's/99999/'${version}'/g' ${DebCtrlDir}/control
+
+    fi
+}
+
 clean() {
     rm -rf target
 }
