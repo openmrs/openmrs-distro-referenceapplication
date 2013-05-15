f97761bd1640b306de86541dfa8ca27c98bde2c8
Chris Briesemeister <breeze@breeze-ubuntu-1204.(none)>
2013-05-15 11:58:14 -0500

    Breeze: RA-46 setting tomcat xmx to 1024

--- puppet/modules/tomcat7/manifests/init.pp
+++ puppet/modules/tomcat7/manifests/init.pp
@@ -4,4 +4,17 @@ include aptitude
     ensure  => "present",
     require => Class[ "aptitude" ],
   }
+  file { "/etc/default/tomcat7" :
+    ensure  => present,
+    mode    => 644,
+    owner   => "root",
+    group   => "root",
+    source  => "puppet:///modules/tomcat7/etc/default/tomcat7",
+    require => Package[ "tomcat7" ],
+    notify  => Service[ "tomcat7"],
+  }
+  service { "tomcat7" :
+    ensure  => running,
+    require => Package[ "tomcat7"],
+  }
 }
