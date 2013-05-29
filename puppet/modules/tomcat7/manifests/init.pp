aca780cf5051905cd6e993eaeb5184e8eaa089a4
Chris Briesemeister <breeze@breeze-ubuntu-1204.(none)>
2013-05-29 13:40:17 -0500

    Breeze moving tomcat7 config file to a template and only using debug mode if on a vagrant box

--- puppet/modules/tomcat7/manifests/init.pp
+++ puppet/modules/tomcat7/manifests/init.pp
@@ -5,13 +5,13 @@ include aptitude
     require => Class[ "aptitude" ],
   }
   file { "/etc/default/tomcat7" :
-    ensure  => present,
-    mode    => 644,
-    owner   => "root",
-    group   => "root",
-    source  => "puppet:///modules/tomcat7/etc/default/tomcat7",
-    require => Package[ "tomcat7" ],
-    notify  => Service[ "tomcat7"],
+    ensure   => present,
+    mode     => 644,
+    owner    => "root",
+    group    => "root",
+    content  => template("tomcat7/etc/default/tomcat7.erb"),
+    require  => Package[ "tomcat7" ],
+    notify   => Service[ "tomcat7"],
   }
   service { "tomcat7" :
     ensure  => running,
