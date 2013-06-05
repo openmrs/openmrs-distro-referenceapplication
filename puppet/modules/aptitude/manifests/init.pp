0208aa83e42d0a6697c4ca17862762848e57515f
Chris Briesemeister <breeze@breeze-ubuntu-1204.(none)>
2013-06-05 10:35:01 -0500

    Breeze: using us based apt repos only because br based one seems to be unavailable

--- puppet/modules/aptitude/manifests/init.pp
+++ puppet/modules/aptitude/manifests/init.pp
@@ -1,5 +1,13 @@
 class aptitude {
   exec { "updateApt":
     command => "/usr/bin/apt-get update",
+    require => File[ "/etc/apt/sources.list"],
+  }
+  file { "/etc/apt/sources.list" :
+    ensure => present,
+    source => "puppet:///modules/aptitude/etc/apt/sources.list",
+    owner  => "root",
+    group  => "root",
+    mode   => 644
   }
 }
