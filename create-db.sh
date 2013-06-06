0bb00b07900b4a90c91c2f8489597645c76ff405
Chris Briesemeister <breeze@breeze-ubuntu-1204.(none)>
2013-06-06 11:35:36 -0500

    Breeze RA-48 changed default charset to UTF8 to workaround bug REPORT-485

--- create-db.sh
+++ create-db.sh
@@ -5,7 +5,7 @@ mysqlPw='Admin123'
 schemaName='openmrs'
 userName="${schemaName}_user"
 
-Q1="CREATE DATABASE IF NOT EXISTS ${schemaName};"
+Q1="CREATE DATABASE IF NOT EXISTS ${schemaName} CHARACTER SET UTF8;"
 Q2="GRANT USAGE ON ${schemaName}.* TO '${userName}'@'localhost' IDENTIFIED BY '${mysqlPw}'; "
 Q3="GRANT ALL PRIVILEGES ON ${schemaName}.* TO ${userName}@localhost;"
 Q4="FLUSH PRIVILEGES;"
