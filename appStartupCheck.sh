ace33e3c9645f2fcb4668300ec30eecb3f2d0797
Chris Briesemeister <breeze@breeze-ubuntu-1204.(none)>
2013-06-13 00:36:35 -0500

    Breeze: RA-80 setting localhost for UI test default and adding a check to remote host for app startup

--- appStartupCheck.sh
+++ appStartupCheck.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
+
+##This needs to get refactored so that there is not so much duplication in each uber if statement
+
 checkForStartup() {
+Host=$1
 
   echo "Waiting for OpenMRS to start.  This could take a few minutes."
   while [ -z "${result}"  ];
     do
-      result=`tail /var/log/tomcat7/catalina.out | grep "INFO: Server startup"`
-      sleep 1
-      count=$((count+1))
-      if [ ${count} == 120 ];
+      if [ "${Host}" == "devtest01.openmrs.org" ]
         then
-        echo "Waited for 2 minutes and Tomcat has not started."
-        echo "Check /var/log/tomcat7/catalina.out to see what is happening."
-        break
+          result=`ssh -o StrictHostKeyChecking=no devtest01.openmrs.org "sudo tail /var/log/tomcat7/catalina.out | grep \"INFO: Server startup\""`
+          sleep 1
+          count=$((count+1))
+          if [ ${count} == 120 ];
+            then
+            echo "Waited for 2 minutes and Tomcat has not started."
+            echo "Check /var/log/tomcat7/catalina.out to see what is happening."
+            exit
+          fi
+        else
+          result=`tail /var/log/tomcat7/catalina.out | grep "INFO: Server startup"`
+          sleep 1
+          count=$((count+1))
+          if [ ${count} == 120 ];
+            then
+            echo "Waited for 2 minutes and Tomcat has not started."
+            echo "Check /var/log/tomcat7/catalina.out to see what is happening."
+            exit
+          fi
       fi
 done
   echo "Tomcat started."
   echo "OpenMRS Reference Application is ready for use."
+
 }
-checkForStartup
+checkForStartup $1
