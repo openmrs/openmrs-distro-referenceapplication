e75c69dc1098e0fe915ec8d92fc60900749cfed3
Chris Briesemeister <breeze@breeze-ubuntu-1204.(none)>
2013-07-31 20:39:31 -0500

    Breeze RA-167

--- utility-scripts/tomcatLogCollector.sh
+++ utility-scripts/tomcatLogCollector.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 workingDir=`pwd`
-remoteExec='ssh -o StrictHostKeyChecking=no breeze@devtest01.openmrs.org'
+remoteExec='ssh -o StrictHostKeyChecking=no devtest01.openmrs.org'
+tomcatLogLocation="/var/log/tomcat7/catalina.out"
 
 cleanTomcatLog(){
   ##Clean out any old logs
   ## In Bamboo's Home Dir on Devtest01 first
-  echo "Hello"
   if ${remoteExec} test -e "~/catalina.out";
     then
       ${remoteExec} "rm ~/catalina.out";
   fi
   ## Then from Bamboos Plan's working Dir
-  if ${remoteExec} test -e "${bamboo.build.working.directory}/catalina.out";
+  if test -e "${workingDir}/catalina.out";
     then
-      echo "here"
-      #${remoteExec} rm "${bamboo.build.working.directory}/catalina.out";
+      rm ${workingDir}/catalina.out
   fi
-
 }
-getTomcatLog() {
-  ${remoteExec} "sudo cp ${tomcatLogLocation} ~"
-  scp -o StrictHostKeyChecking=no breeze@devtest01.openmrs.org:~/catalina.out .
 
+getTomcatLog() {
+  ${remoteExec} "sudo cp ${tomcatLogLocation} ~/"
+  scp -o StrictHostKeyChecking=no devtest01.openmrs.org:~/catalina.out ${workingDir}
 }
 
 $1
