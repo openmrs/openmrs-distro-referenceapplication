8aa574cef49085c7c93e5acf0887971048ee5ff7
Chris Briesemeister <breeze@breeze-ubuntu-1204.(none)>
2013-09-12 09:45:24 -0500

    Breeze: RA-127 removing some duplication

--- utility-scripts/create-db.sh
+++ utility-scripts/create-db.sh
@@ -6,14 +6,14 @@ schemaName='openmrs'
 userName="${schemaName}_user"
 
 Q1="CREATE DATABASE IF NOT EXISTS ${schemaName} CHARACTER SET UTF8;"
-Q2="GRANT USAGE ON ${schemaName}.* TO '${userName}'@'localhost' IDENTIFIED BY '${mysqlPw}'; "
-#Q3="GRANT ALL PRIVILEGES ON ${schemaName}.* TO ${userName}@'%';"
-Q3="GRANT ALL PRIVILEGES ON openmrs.* TO root@'%' IDENTIFIED BY 'Admin123';;"
-Q4="FLUSH PRIVILEGES;"
-SQL="${Q1}${Q2}${Q3}${Q4}"
+Q2="GRANT USAGE ON ${schemaName}.* TO '${userName}'@'localhost' IDENTIFIED BY '${mysqlPw}';"
+Q3="GRANT ALL PRIVILEGES ON ${schemaName}.* TO ${userName}@'%';"
+Q4="GRANT ALL PRIVILEGES ON ${schemaName}.* TO root@'%' IDENTIFIED BY '${mysqlPw}';"
+Q5="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}${Q4}${Q5}"
 
 #Create the OpenMRS Schema
 $MYSQL -uroot -p${mysqlPw} -e "${SQL}"
 
 #Set the privs for it
-$MYSQL -uroot -p${mysqlPw} -e "GRANT ALL PRIVILEGES ON openmrs.* TO root@'%' IDENTIFIED BY '${mysqlPw}';"
+#$MYSQL -uroot -p${mysqlPw} -e "GRANT ALL PRIVILEGES ON openmrs.* TO root@'%' IDENTIFIED BY '${mysqlPw}';"
