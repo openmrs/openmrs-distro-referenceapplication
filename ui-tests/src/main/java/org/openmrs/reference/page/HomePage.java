c880ee155702bf37f41126fa289a0134916a0e1b
Izaron <izarizar@mail.ru>
2015-01-02 10:44:59 +0300

    GCI-4 - Session Location Test
    This code checking the existence of the location name in the text
    "Logged as"
    after login
    Melange task:
    https://www.google-melange.com/gci/task/view/google/gci2014/4822021892472832

--- ui-tests/src/main/java/org/openmrs/reference/page/HomePage.java
+++ ui-tests/src/main/java/org/openmrs/reference/page/HomePage.java
@@ -1,8 +1,22 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.reference.page;
 
 import org.openmrs.uitestframework.page.AbstractBasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomePage extends AbstractBasePage {
 	
@@ -77,6 +91,12 @@ public class HomePage extends AbstractBasePage {
 	public boolean isCaptureVitalsAppPresent() {
 		return isAppButtonPresent(CAPTURE_VITALS_APP_ID);
     }
+	
+	public String getLoggedAsString() {
+		WebElement container = driver.findElement(By.id("home-container"));
+		WebElement text = container.findElement(By.xpath("id('home-container')/h4"));
+		return text.getText();
+	}
 
 	@Override
 	public String expectedUrlPath() {
