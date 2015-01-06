02c6694df65900340eaad0f963a872a4d4aa8bd7
Izaron <izarizar@mail.ru>
2015-01-06 20:50:11 +0300

    GCI-4 - Style Guide Colors Test
    Style Guide Colors Test
    Melange task:
    https://www.google-melange.com/gci/task/view/google/gci2014/5330144977747968

--- ui-tests/src/main/java/org/openmrs/reference/page/PatientDashboardPage.java
+++ ui-tests/src/main/java/org/openmrs/reference/page/PatientDashboardPage.java
@@ -1,7 +1,23 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.reference.page;
+
+import java.util.List;
 
 import org.openmrs.uitestframework.page.AbstractBasePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -14,17 +30,176 @@ public class PatientDashboardPage extends AbstractBasePage {
 	private static final By CONFIRM = By.cssSelector("#quick-visit-creation-dialog .confirm");
 	private static final By STARTED_AT = By.className("active-visit-started-at-message");
 	private static final By VISIT_NOTE = By.id("referenceapplication.realTime.simpleVisitNote");
+	private static final By CAPTURE_VITALS = By.id("referenceapplication.realTime.vitals");
 	private static final By DIAGNOSIS_SEARCH_CONTAINER = By.id("diagnosis-search-container");
 	private static final By DIAGNOSIS_SEARCH = By.id("diagnosis-search");
-
+	private static final By CONTENT = By.id("content");
+	
+	private static final By VITALS_HEIGHT = By.id("height");
+	private static final By VITALS_WEIGHT = By.id("weight");
+	private static final By VITALS_TEMPERATURE = By.id("temperature");
+	private static final By VITALS_HEART_RATE = By.id("heart_rate");
+	private static final By VITALS_RESPIRATORY_RATE = By.id("respiratory_rate");
+	private static final By VITALS_BP_SYSTOLIC = By.id("bp_systolic");
+	private static final By VITALS_BP_DIASTOLIC = By.id("bp_diastolic");
+	private static final By VITALS_OXYGEN_SATURATION = By.id("o2_sat");
+	
 	public PatientDashboardPage(WebDriver driver) {
 	    super(driver);
     }
+	
+	public void editDemographics() {
+		WebElement content = driver.findElement(CONTENT);
+		WebElement editButton = content.findElement(By.xpath("id('content')/div[5]/div[1]/h1/span[3]/span[3]"));
+		editButton.click();
+	}
+	
+	public void editContactInfo() {
+		WebElement contactInfoContent = driver.findElement(By.id("contactInfoContent"));
+		WebElement editContactInfoButton = contactInfoContent.findElement(By.xpath("id('contactInfoContent')/div/small"));
+		editContactInfoButton.click();
+	}
+	
+	public String getGivenName() {
+		WebElement content = driver.findElement(CONTENT);
+		WebElement span = content.findElement(By.xpath("id('content')/div[5]/div[1]/h1/span[2]"));
+		return span.getText().split("\\s+")[0];
+	}
+	
+	public String getFamilyName() {
+		WebElement content = driver.findElement(CONTENT);
+		WebElement span = content.findElement(By.xpath("id('content')/div[5]/div[1]/h1/span[1]"));
+		String family = span.getText().split("\\s+")[0];
+		family = family.substring(0, family.length() - 1);	// delete last character ','
+		return family;
+	}
+	
+	public String getGender() {
+		WebElement content = driver.findElement(CONTENT);
+		WebElement span = content.findElement(By.xpath("id('content')/div[5]/div[1]/h1/span[3]/span[1]"));
+		return span.getText();
+	}
+
+	public String[] getBirthdateData() {
+		WebElement content = driver.findElement(CONTENT);
+		WebElement span = content.findElement(By.xpath("id('content')/div[5]/div[1]/h1/span[3]/span[2]"));
+		
+		String data = span.getText().split("\\s+")[2];
+		data = data.substring(1, data.length() - 1);
+		
+		return data.split("\\.");
+	}
+	
+	public String[] getAddressData() {
+		WebElement contactInfoContent = driver.findElement(By.id("contactInfoContent"));
+		WebElement span = contactInfoContent.findElement(By.xpath("id('contactInfoContent')/div/span[1]"));
+		
+		String[] address = span.getText().split("\\s+");
+		
+		address[0] = address[0].substring(0, address[0].length() - 1);	// remove ','
+		address[1] = address[1].substring(0, address[1].length() - 1);	// remove ','
+		
+		return address;
+	}
+	
+	public String getPhoneNumber() {
+		WebElement contactInfoContent = driver.findElement(By.id("contactInfoContent"));
+		WebElement span = contactInfoContent.findElement(By.xpath("id('contactInfoContent')/div/span[2]"));
+		String[] text = span.getText().split("\\s+");
+		return text[0];
+	}
+	
+	public void showContactInfo() {
+		WebElement showContactInfoButton = driver.findElement(By.id("patient-header-contactInfo"));
+		showContactInfoButton.click();
+	}
 
 	public void startVisit() {
 		clickOn(START_VISIT);
 		waitForElement(CONFIRM);
 		clickOn(CONFIRM);
+	}
+	
+	public void captureVitals() {
+		clickOn(CAPTURE_VITALS);
+	}
+	
+	public void clickVisitAction(int index) {
+		WebElement content = driver.findElement(CONTENT);
+		List<WebElement> liCollection = content.findElements(By.xpath("id('content')/div[8]/div/div[3]/div[1]/ul[1]/li"));
+		liCollection.get(index).click();
+	}
+	
+	public void clickGeneralAction(int index) {
+		WebElement content = driver.findElement(CONTENT);
+		List<WebElement> aCollection = content.findElements(By.xpath("id('content')/div[8]/div/div[3]/div[1]/ul[2]/a"));
+		aCollection.get(index).click();
+	}
+	
+	public void editAllergies(String currPage) {
+		currPage = currPage.substring(currPage.indexOf('?') + 1);
+		((JavascriptExecutor) driver).executeScript("location.href='/openmrs/allergyui/allergies.page?" + currPage + "';");
+	}
+	
+	public String getAllergen() {
+		WebElement span = driver.findElement(By.className("allergen"));
+		return span.getText();
+	}
+	
+	public String getAllergyReaction() {
+		WebElement span = driver.findElement(By.className("allergyReaction"));
+		return span.getText();
+	}
+	
+	public String getAllergyReaction(int index) {
+		List<WebElement> span = driver.findElements(By.className("allergyReaction"));
+		return span.get(index).getText();
+	}
+	
+	public String getAllergiesString() {
+		WebElement content = driver.findElement(CONTENT);
+		WebElement span = content.findElement(By.xpath("id('content')/div[8]/div/div[2]/div[2]/div[2]"));
+		return span.getText();
+	}
+	
+	public double getVitalsHeight() {
+		WebElement span = driver.findElement(VITALS_HEIGHT).findElement(By.xpath("id('height')/span"));
+		return Double.parseDouble(span.getText());
+	}
+	
+	public double getVitalsWeight() {
+		WebElement span = driver.findElement(VITALS_WEIGHT).findElement(By.xpath("id('weight')/span"));
+		return Double.parseDouble(span.getText());
+	}
+	
+	public double getVitalsTemperature() {
+		WebElement span = driver.findElement(VITALS_TEMPERATURE).findElement(By.xpath("id('temperature')/span"));
+		return Double.parseDouble(span.getText());
+	}
+	
+	public int getVitalsHeartRate() {
+		WebElement span = driver.findElement(VITALS_HEART_RATE).findElement(By.xpath("id('heart_rate')/span"));
+		return Integer.parseInt(span.getText());
+	}
+	
+	public long getVitalsRespiratoryRate() {
+		WebElement span = driver.findElement(VITALS_RESPIRATORY_RATE).findElement(By.xpath("id('respiratory_rate')/span"));
+		return Long.parseLong(span.getText());
+	}
+	
+	public int getVitalsBpSystolic() {
+		WebElement span = driver.findElement(VITALS_BP_SYSTOLIC).findElement(By.xpath("id('bp_systolic')/span"));
+		return Integer.parseInt(span.getText());
+	}
+	
+	public int getVitalsBpDiastolic() {
+		WebElement span = driver.findElement(VITALS_BP_DIASTOLIC).findElement(By.xpath("id('bp_diastolic')/span"));
+		return Integer.parseInt(span.getText());
+	}
+	
+	public int getVitalsOxygenSaturation() {
+		WebElement span = driver.findElement(VITALS_OXYGEN_SATURATION).findElement(By.xpath("id('o2_sat')/span"));
+		return Integer.parseInt(span.getText());
 	}
 	
 	@Override
---
+++ ui-tests/src/test/java/org/openmrs/reference/StyleColorTest.java
@@ -0,0 +1,273 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.reference;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.reference.page.AllergiesDashboardPage;
+import org.openmrs.reference.page.FindPatientRecordPage;
+import org.openmrs.reference.page.HeaderPage;
+import org.openmrs.reference.page.HomePage;
+import org.openmrs.reference.page.PatientDashboardPage;
+import org.openmrs.uitestframework.test.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Color;
+
+public class StyleColorTest extends TestBase {
+	private HeaderPage headerPage;
+	private HomePage homePage;
+	private PatientDashboardPage patientDashboardPage;
+	private FindPatientRecordPage findPatientRecordPage;
+	private AllergiesDashboardPage allergiesDashboardPage;
+	
+	private List<String> colors;
+	private List<WebElement> allFormChildElements;
+	
+	// The colors that are considered normal
+	// Anyone can change the list by adding or removing color
+	private static final String[] NORMAL_COLORS = {
+		
+		"#000000", "#ffffff",	// Black and white colors
+		
+		"#f26522", "#5b57a6", "#eea616", "#009384", "#231f20",	// OpenMRS Colors
+		
+		"#363463", "#007fff", "#003366",	// Others very repeated colors
+		
+		"#f9f9f9", "#fffdf7", "#f3f3f3", "#fafafa",		// Backgrounds colors
+		
+		"#aaaaaa", "#bbbbbb", "#cccccc", "#dddddd", "#eeeeee",	// Shades of gray
+		"#222222", "#333333", "#555555", "#666666", "#888888", "#999999",
+		"#808080", "#d8d8d8", "#e2e2e2", "#e6e6e6"
+		
+	};
+	
+	@Before
+	public void before() {
+		headerPage = new HeaderPage(driver);
+        homePage = new HomePage(driver);
+        patientDashboardPage = new PatientDashboardPage(driver);
+        findPatientRecordPage = new FindPatientRecordPage(driver);
+        allergiesDashboardPage = new AllergiesDashboardPage(driver);
+        
+        login();
+		assertPage(homePage);
+	}
+	
+	@After
+	public void after() {
+		homePage.go();
+		assertPage(homePage);
+		
+		headerPage.logOut();
+		assertPage(loginPage);
+	}
+	
+	// Anyone can add new page for testing
+	@Test
+	public void homePageTest() {
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void loginPageTest() {
+		homePage.go();
+		assertPage(homePage);
+		
+		headerPage.logOut();
+		assertPage(loginPage);
+		
+		checkWebPageAllElementsColor();
+		
+		login();
+		assertPage(homePage);
+	}
+	
+	@Test
+	public void adminPageTest() {
+		currentPage().gotoPage("/admin/index.htm");
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void findPatientRecordPageTest() {
+		currentPage().gotoPage("/coreapps/findpatient/findPatient.page?app=coreapps.findPatient");
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void patientDashboardPageTest() {
+		currentPage().gotoPage("/coreapps/findpatient/findPatient.page?app=coreapps.findPatient");
+		findPatientRecordPage.clickFirstPatient();
+		assertPage(patientDashboardPage);
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void patientAllergiesPageTest() {
+		currentPage().gotoPage("/coreapps/findpatient/findPatient.page?app=coreapps.findPatient");
+		findPatientRecordPage.clickFirstPatient();
+		
+		assertPage(patientDashboardPage);
+		patientDashboardPage.editAllergies(driver.getCurrentUrl());
+		
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void patientEditAllergyPageTest() {
+		currentPage().gotoPage("/coreapps/findpatient/findPatient.page?app=coreapps.findPatient");
+		findPatientRecordPage.clickFirstPatient();
+		
+		assertPage(patientDashboardPage);
+		patientDashboardPage.editAllergies(driver.getCurrentUrl());
+		
+		allergiesDashboardPage.addAllergy(driver.getCurrentUrl());
+		
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void patientEditDemographicsPageTest() {
+		currentPage().gotoPage("/coreapps/findpatient/findPatient.page?app=coreapps.findPatient");
+		findPatientRecordPage.clickFirstPatient();
+		
+		assertPage(patientDashboardPage);
+		patientDashboardPage.editDemographics();
+		
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void patientEditContactInfoPageTest() {
+		currentPage().gotoPage("/coreapps/findpatient/findPatient.page?app=coreapps.findPatient");
+		findPatientRecordPage.clickFirstPatient();
+		
+		assertPage(patientDashboardPage);
+		patientDashboardPage.showContactInfo();
+		patientDashboardPage.editContactInfo();
+		
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void visitNotePageTest() {
+		currentPage().gotoPage("/coreapps/findpatient/findPatient.page?app=coreapps.findPatient");
+		findPatientRecordPage.clickFirstPatient();
+		
+		assertPage(patientDashboardPage);
+		patientDashboardPage.clickVisitAction(1);
+		
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void exitFromInpatientPageTest() {
+		currentPage().gotoPage("/coreapps/findpatient/findPatient.page?app=coreapps.findPatient");
+		findPatientRecordPage.clickFirstPatient();
+		
+		assertPage(patientDashboardPage);
+		patientDashboardPage.clickVisitAction(2);
+		
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void transferToWardServicePageTest() {
+		currentPage().gotoPage("/coreapps/findpatient/findPatient.page?app=coreapps.findPatient");
+		findPatientRecordPage.clickFirstPatient();
+		
+		assertPage(patientDashboardPage);
+		patientDashboardPage.clickVisitAction(3);
+		
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void mergeVisitsPageTest() {
+		currentPage().gotoPage("/coreapps/findpatient/findPatient.page?app=coreapps.findPatient");
+		findPatientRecordPage.clickFirstPatient();
+		
+		assertPage(patientDashboardPage);
+		patientDashboardPage.clickGeneralAction(1);
+		
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void findActiveVisitsPageTest() {
+		currentPage().gotoPage("/coreapps/activeVisits.page?app=coreapps.activeVisits");
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void registerPatientPageTest() {
+		currentPage().gotoPage("/registrationapp/registerPatient.page?appId=referenceapplication.registrationapp.registerPatient");
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void captureVitalsPageTest() {
+		currentPage().gotoPage("/coreapps/findpatient/findPatient.page?app=referenceapplication.vitals");
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void atlasPageTest() {
+		currentPage().gotoPage("/atlas/map.page");
+		checkWebPageAllElementsColor();
+	}
+	
+	@Test
+	public void styleGuidePageTest() {
+		currentPage().gotoPage("/uicommons/styleGuide.page");
+		checkWebPageAllElementsColor();
+	}
+	
+	public String getElementXPath(WebDriver driver, WebElement element) {
+	    return (String)((JavascriptExecutor)driver).executeScript("gPt=function(c){if(c.id!==''){return'id(\"'+c.id+'\")'}if(c===document.body){return c.tagName}var a=0;var e=c.parentNode.childNodes;for(var b=0;b<e.length;b++){var d=e[b];if(d===c){return gPt(c.parentNode)+'/'+c.tagName+'['+(a+1)+']'}if(d.nodeType===1&&d.tagName===c.tagName){a++}}};return gPt(arguments[0]).toLowerCase();", element);
+	}
+	
+	private void checkWebPageAllElementsColor() {
+		// print page url
+		System.out.println("\n\n" + driver.getCurrentUrl() + "\n");
+		
+		// get all elements
+		allFormChildElements = driver.findElements(By.cssSelector("*"));
+		// get all normal colors
+		colors = Arrays.asList(NORMAL_COLORS);
+		
+		for(WebElement item : allFormChildElements)
+		{
+			String color = Color.fromString(item.getCssValue("color")).asHex();
+			String backgroundColor = Color.fromString(item.getCssValue("background-color")).asHex();
+			
+			// check "color" css value
+			if(color != "" && !colors.contains(color)) {
+				System.out.println("Weird color: " + color + ", Item path: " + getElementXPath(driver, item));
+			}
+			
+			// check "background-color" css value
+			if(backgroundColor != "" && !colors.contains(backgroundColor)) {
+				System.out.println("Weird background color: " + backgroundColor + ", Item path: " + getElementXPath(driver, item));
+			}
+		}
+	}
+}
