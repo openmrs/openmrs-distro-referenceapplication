4d77338694e4117a9fbc6d8bd98678d9c0ffec00
Natalia Plonka <nplonka@soldevelo.com>
2015-07-15 13:01:54 +0200

    RA-837 Automating: RA-755 Add Location in Location Management; RA-756 Delete Location in Location Management

---
+++ ui-tests/src/test/java/org/openmrs/reference/AddLocationTest.java
@@ -0,0 +1,61 @@
+package org.openmrs.reference;
+
+/**
+ * Created by nata on 15.07.15.
+ */
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import org.openmrs.reference.page.HeaderPage;
+import org.openmrs.reference.page.HomePage;
+import org.openmrs.reference.page.LocationPage;
+import org.openmrs.reference.page.PatientDashboardPage;
+import org.openmrs.uitestframework.test.TestBase;
+
+
+public class AddLocationTest extends TestBase {
+    private HomePage homePage;
+    private PatientDashboardPage patientDashboardPage;
+    private HeaderPage headerPage;
+    private LocationPage locationPage;
+
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        homePage = new HomePage(driver);
+        loginPage.loginAsAdmin();
+        assertPage(homePage);
+        patientDashboardPage = new PatientDashboardPage(driver);
+        headerPage = new HeaderPage(driver);
+        locationPage = new LocationPage(driver);
+        homePage.goToAdministration();
+    }
+
+    @Test
+    public void addLocationTest() throws Exception {
+        locationPage.clickOnManageLocation();
+        if(!locationPage.locationPresent()) {
+            locationPage.clickOnCheckLocation();
+            locationPage.clickOnDelete();
+        }
+        locationPage.clickOnAddLocation();
+        locationPage.clickOnSaveLocation();
+        assertTrue(driver.getPageSource().contains("Please fix all errors and try again."));
+        locationPage.enterName("psychiatric hospital");
+        locationPage.chooseTags("Admission Location");
+        locationPage.clickOnTags();
+        locationPage.clickOnSaveLocation();
+        locationPage.clickOnCheckLocation();
+        locationPage.clickOnDelete();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        headerPage.clickOnHomeLink();
+        headerPage.logOut();
+    }
+
+}
