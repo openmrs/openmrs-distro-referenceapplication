24851f348f86e9424eb3d3491879470acb85b861
Tomasz Mueller <tmueller@soldevelo.com>
2015-07-24 15:27:45 +0200

    RA-882 "Automating RA-721 Record Merge Issue Test"

---
+++ ui-tests/src/test/java/org/openmrs/reference/RecordMergeIssueTest.java
@@ -0,0 +1,90 @@
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.reference.helper.TestPatient;
+import org.openmrs.reference.page.*;
+import org.openmrs.uitestframework.test.TestBase;
+import org.junit.*;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by tomasz on 09.07.15.
+ */
+
+public class RecordMergeIssueTest extends TestBase {
+    private HomePage homePage;
+    private HeaderPage headerPage;
+    private FindPatientPage findPatientPage;
+    private TestPatient patient;
+    private TestPatient patient1;
+    private RegistrationPage registrationPage;
+    private PatientDashboardPage patientDashboardPage;
+    private DataManagementPage dataManagementPage;
+    private String id;
+    private String id2;
+
+
+    @Before
+    public void setUp() throws Exception {
+        homePage = new HomePage(driver);
+        loginPage.loginAsAdmin();
+        assertPage(homePage);
+        headerPage = new HeaderPage(driver);
+        findPatientPage = new FindPatientPage(driver);
+        registrationPage = new RegistrationPage(driver);
+        patientDashboardPage = new PatientDashboardPage(driver);
+        dataManagementPage = new DataManagementPage(driver);
+        patient = new TestPatient();
+        patient1 = new TestPatient();
+
+
+    }
+
+    @Ignore
+    @Test
+    public void recordMergeIssueTest() throws Exception {
+        homePage.openRegisterAPatientApp();
+//       Register first patient
+        patient.familyName = "Mike";
+        patient.givenName = "Smith";
+        patient.gender = "Male";
+        patient.estimatedYears = "25";
+        patient.address1 = "address";
+        registrationPage.enterMegrePatient(patient);
+        id = patientDashboardPage.findPatientId();
+        patient.Uuid = patientIdFromUrl();
+        headerPage.clickOnHomeIcon();
+//     Register second patient
+        homePage.openRegisterAPatientApp();
+        patient1.familyName = "Mike";
+        patient1.givenName = "Kowalski";
+        patient1.gender = "Male";
+        patient1.estimatedYears = "25";
+        patient1.address1 = "address";
+        registrationPage.enterMegrePatient(patient1);
+        id2 = patientDashboardPage.findPatientId();
+        patient1.Uuid = patientIdFromUrl();
+        headerPage.clickOnHomeIcon();
+        homePage.goToDataMagament();
+        dataManagementPage.goToMegrePatient();
+        dataManagementPage.enterPatient1(id);
+        dataManagementPage.enterPatient2(id2);
+        dataManagementPage.clickOnContinue();
+        Thread.sleep(300);
+        assertFalse(driver.getPageSource().contains("java.lang.NullPointerException"));
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        headerPage.clickOnHomeIcon();
+//        deletePatient(patient.Uuid);
+//        waitForPatientDeletion(patient.Uuid);
+//        deletePatient(patient1.Uuid);
+//        waitForPatientDeletion(patient1.Uuid);
+        headerPage.logOut();
+    }
+}
