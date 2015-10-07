feecbb7d52c3a0f4d466976350b54394e0bb6aed
yulia <yuliageek@gmail.com>
2015-10-06 23:03:06 -0700

    Replace WebDriverWait by Thread.sleep() for waiting result table

--- ui-tests/src/main/java/org/openmrs/reference/page/FindPatientPage.java
+++ ui-tests/src/main/java/org/openmrs/reference/page/FindPatientPage.java
@@ -8,18 +8,15 @@ package org.openmrs.reference.page;
 import org.openmrs.uitestframework.page.AbstractBasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FindPatientPage extends AbstractBasePage {
 
-    public static final String DATA_TABLES_EMPTY = ".dataTables_empty";
     public static final By PATIENT_SEARCH_RESULT = By.id("patient-search-results-table");
     public static final By PATIENT_NAME_SEARCH_RESULT = By.xpath("//table[@id='patient-search-results-table']/tbody/tr/td[2]");
     public static final By PATIENT_ID_SEARCH_RESULT = By.xpath("//table[@id='patient-search-results-table']/tbody/tr/td[1]");
     private static final By PATIENT_SEARCH = By.id("patient-search");
     private static final By PATIENT = By.xpath("//table[@id='patient-search-results-table']/tbody/tr/td[2]");
+    public static final int SLEEP_TIME = 1000; //1 second
 
     public FindPatientPage(WebDriver driver) {
         super(driver);
@@ -40,9 +37,6 @@ public class FindPatientPage extends AbstractBasePage {
         return findElement(PATIENT_ID_SEARCH_RESULT).getText();
     }
 
-    public WebElement nameSearchResult() {
-        return findElement(PATIENT_SEARCH_RESULT);
-    }
 
     
 
@@ -54,11 +48,14 @@ public class FindPatientPage extends AbstractBasePage {
     /**
      * waits for results appear in a table
      */
-    public void waitForResultTable() {
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        //wait for table to be cleared from previous results
-        wait.until(ExpectedConditions.visibilityOf(findElement(By.cssSelector(DATA_TABLES_EMPTY))));
-        //wait for the first row of the table
-        wait.until(ExpectedConditions.visibilityOf(findElement(PATIENT_NAME_SEARCH_RESULT)));
+    public void waitForResultTable() throws InterruptedException {
+        int sleepTime = SLEEP_TIME;
+        while (sleepTime < SLEEP_TIME * 10) {
+            Thread.sleep(sleepTime);
+            if (findElement(PATIENT_NAME_SEARCH_RESULT) != null) {
+                return;
+            }
+            sleepTime = sleepTime * 2;
+        }
     }
 }
