45bf7de3e7661ddaa9ae1fc9a7c4ef1233eb08ce
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-10-08 16:50:22 +0200

    Fixing AddRolesToUserTest

--- ui-tests/src/test/java/org/openmrs/reference/AddRolesToUserTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/AddRolesToUserTest.java
@@ -1,35 +1,33 @@
 package org.openmrs.reference;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.openmrs.reference.page.*;
-import org.openmrs.uitestframework.test.TestBase;
-import org.openmrs.uitestframework.test.TestData;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Random;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.reference.page.AdministrationPage;
+import org.openmrs.reference.page.HeaderPage;
+import org.openmrs.reference.page.HomePage;
+import org.openmrs.reference.page.ManageUserPage;
+import org.openmrs.uitestframework.test.TestBase;
 
 /**
  * Created by tomasz on 10.07.15.
  */
 public class AddRolesToUserTest extends TestBase {
 
-    private HomePage homePage;
+    private static final String NURSE_PASSWORD = "Nurse321";
+	private static final String NURSE_USERNAME = "newNurse" + new Random().nextInt(1024);
+	private HomePage homePage;
     private HeaderPage headerPage;
     private AdministrationPage administrationPage;
     private ManageUserPage manageUserPage;
 
     @Before
     public void setUp() throws Exception {
-
         loginPage.loginAsAdmin();
         homePage = new HomePage(driver);
         assertPage(homePage);
@@ -42,7 +40,7 @@ public class AddRolesToUserTest extends TestBase {
     private void reLoginAsUser() throws InterruptedException {
         manageUserPage.clickOnHomeLink();
         headerPage.logOut();
-        loginPage.login("super_nurse", "Nurse321");
+        loginPage.login(NURSE_USERNAME, NURSE_PASSWORD);
     }
 
     private void reLoginAsAdmin() throws InterruptedException {
@@ -58,24 +56,23 @@ public class AddRolesToUserTest extends TestBase {
         Map<String,Integer> roleModules = new HashMap();
 
         fillInRoleModules(roleModules);
-        if(!manageUserPage.userExists("super_nurse")) {
+        if(!manageUserPage.userExists(NURSE_USERNAME)) {
             manageUserPage.clickOnAddUser();
             manageUserPage.createNewPerson();
-            manageUserPage.fillInPersonName("Super", "Nurse", "super_nurse", "Nurse321");
+            manageUserPage.fillInPersonName("Super", "Nurse", NURSE_USERNAME, NURSE_PASSWORD);
         }
+        
         String oldRole = null;
         for(Entry<String, Integer> role : roleModules.entrySet()) {
-            manageUserPage.assignRolesToUser(oldRole,role.getKey(),"super_nurse");
+            manageUserPage.assignRolesToUser(oldRole,role.getKey(),NURSE_USERNAME);
             reLoginAsUser();
             if(homePage.numberOfAppsPresent() != role.getValue()) {
                 throw new AssertionError("role "+role+" doesn't have matching number of accessible applications: should be:"+role.getValue()+"is:"+homePage.numberOfAppsPresent());
             }
-//            assertThat(homePage.numberOfAppsPresent(), is(role.getValue()));
             reLoginAsAdmin();
             oldRole = role.getKey();
             homePage.goToAdministration();
             administrationPage.clickOnManageUsers();
-
         }
 
     }
@@ -106,9 +103,10 @@ public class AddRolesToUserTest extends TestBase {
         roleModules.put("roleStrings.Organizational:Nurse", 4);
         roleModules.put("roleStrings.Organizational:RegistrationClerk", 4);
     }
+    
     @After
     public void tearDown() throws Exception {
-        manageUserPage.removeUser("super_nurse");
+        manageUserPage.removeUser(NURSE_USERNAME);
         manageUserPage.clickOnHomeLink();
         headerPage.logOut();
     }
