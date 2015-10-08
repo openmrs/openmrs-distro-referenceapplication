86c7154ede85ac6e0ac3d1cf3c8ca0df7b5de3c0
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-10-08 18:53:51 +0200

    Fixed user password test

--- ui-tests/src/test/java/org/openmrs/reference/PasswordUpperAndLowerCaseTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/PasswordUpperAndLowerCaseTest.java
@@ -3,69 +3,83 @@ package org.openmrs.reference;
 /**
  * Created by nata on 22.07.15.
  */
+import java.util.Random;
+
 import org.junit.*;
+
 import static org.junit.Assert.*;
+
 import org.openmrs.reference.page.*;
 import org.openmrs.uitestframework.test.TestBase;
 
-
-
 public class PasswordUpperAndLowerCaseTest extends TestBase {
-    private HomePage homePage;
-    private HeaderPage headerPage;
-    private SettingPage settingPage;
-    private AdministrationPage administrationPage;
-    private ManageUserPage manageUserPage;
+	
+	public static final String DOCTOR_USERNAME = "dr_house" + new Random().nextInt(1024);
 
-
-    @Before
-    public void setUp() throws Exception {
-
-        loginPage.loginAsAdmin();
-        homePage = new HomePage(driver);
-        assertPage(homePage);
-        headerPage = new HeaderPage(driver);
-        settingPage = new SettingPage(driver);
-        administrationPage = new AdministrationPage(driver);
-        manageUserPage = new ManageUserPage(driver);
-        homePage.goToAdministration();
-    }
-
-    @Test
-    public void passwordUpperAndLowerCaseTest() throws Exception {
-        settingPage.clickOnSetting();
-        settingPage.clickOnSecurity();
-        settingPage.chooseFalseCase();
-        settingPage.saveProperties();
-        headerPage.clickOnHomeLink();
-        homePage.goToAdministration();
-        administrationPage.clickOnManageUsers();
-        manageUserPage.checkUser("dr_house");
-        if (manageUserPage.userExist("dr_house")) {
-            manageUserPage.clickOnUser();
-            manageUserPage.deleteUser();
-        }
-            manageUserPage.clickOnAddUser();
-            manageUserPage.createNewPerson();
-            manageUserPage.enterUserMale("doctor", "House", "dr_house", "drhouse1");
-            manageUserPage.chooseRole();
-            manageUserPage.saveUser();
-            manageUserPage.findUser("dr_house");
-            manageUserPage.deleteUser();
-            headerPage.clickOnHomeLink();
-            homePage.goToAdministration();
-            settingPage.clickOnSetting();
-            settingPage.clickOnSecurity();
-            settingPage.chooseTrueCase1();
-            settingPage.waitForMessage();
-            assertTrue(driver.getPageSource().contains("Global properties saved"));
-        }
-
-    @After
-    public void tearDown() throws Exception {
-        headerPage.clickOnHomeLink();
-        headerPage.logOut();
-    }
-
+	private HomePage homePage;
+	
+	private HeaderPage headerPage;
+	
+	private SettingPage settingPage;
+	
+	private AdministrationPage administrationPage;
+	
+	private ManageUserPage manageUserPage;
+	
+	@Before
+	public void setUp() throws Exception {
+		loginPage.loginAsAdmin();
+		homePage = new HomePage(driver);
+		assertPage(homePage);
+		headerPage = new HeaderPage(driver);
+		settingPage = new SettingPage(driver);
+		administrationPage = new AdministrationPage(driver);
+		manageUserPage = new ManageUserPage(driver);
+		homePage.goToAdministration();
+	}
+	
+	@Test
+	public void passwordUpperAndLowerCaseTest() throws Exception {
+		settingPage.clickOnSetting();
+		settingPage.clickOnSecurity();
+		settingPage.chooseFalseCase();
+		settingPage.saveProperties();
+		headerPage.clickOnHomeLink();
+		homePage.goToAdministration();
+		administrationPage.clickOnManageUsers();
+		manageUserPage.checkUser(DOCTOR_USERNAME);
+		if (manageUserPage.userExist(DOCTOR_USERNAME)) {
+			manageUserPage.clickOnUser();
+			manageUserPage.deleteUser();
+		}
+		manageUserPage.clickOnAddUser();
+		manageUserPage.createNewPerson();
+		manageUserPage.enterUserMale("doctor", "House", DOCTOR_USERNAME, "drhouse1");
+		manageUserPage.chooseRole();
+		manageUserPage.saveUser();
+		manageUserPage.findUser(DOCTOR_USERNAME);
+		manageUserPage.deleteUser();
+		headerPage.clickOnHomeLink();
+		homePage.goToAdministration();
+		settingPage.clickOnSetting();
+		settingPage.clickOnSecurity();
+		settingPage.chooseTrueCase1();
+		settingPage.waitForMessage();
+		assertTrue(driver.getPageSource().contains("Global properties saved"));
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		headerPage.clickOnHomeLink();
+		homePage.goToAdministration();
+		administrationPage.clickOnManageUsers();
+		manageUserPage.checkUser(DOCTOR_USERNAME);
+		if (manageUserPage.userExist(DOCTOR_USERNAME)) {
+			manageUserPage.clickOnUser();
+			manageUserPage.deleteUser();
+		}
+		headerPage.clickOnHomeLink();
+		headerPage.logOut();
+	}
+	
 }
-
