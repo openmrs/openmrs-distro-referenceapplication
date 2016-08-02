073fa53b2a2c5a820408773c2e7cce0847460efe
dkayiwa <dkayiwa@openmrs.org>
2016-08-02 13:26:12 +0300

    Revert "RA-1110: Fix EditDemographicTest"

--- ui-tests/src/test/java/org/openmrs/reference/EditDemographicTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/EditDemographicTest.java
@@ -1,62 +1,75 @@
-/**
- * This Source Code Form is subject to the terms of the Mozilla Public License,
- * v. 2.0. If a copy of the MPL was not distributed with this file, You can
- * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
- * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- *
- * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
- * graphic logo is a trademark of OpenMRS Inc.
- */
 package org.openmrs.reference;
 
+/**
+ * Created by nata on 22.06.15.
+ */
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.openmrs.reference.groups.BuildTests;
-import org.openmrs.reference.helper.PatientGenerator;
 import org.openmrs.reference.helper.TestPatient;
+import org.openmrs.reference.page.HeaderPage;
+import org.openmrs.reference.page.HomePage;
 import org.openmrs.reference.page.ClinicianFacingPatientDashboardPage;
-import org.openmrs.reference.page.EditDemographicsPage;
+import org.openmrs.reference.page.RegistrationPage;
+import org.openmrs.uitestframework.test.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 
-public class EditDemographicTest extends ReferenceApplicationTestBase {
-    private EditDemographicsPage editDemographicsPage;
+public class EditDemographicTest extends TestBase {
+    private StringBuffer verificationErrors = new StringBuffer();
+    private HomePage homePage;
+    private HeaderPage headerPage;
+    private RegistrationPage registrationPage;
     private ClinicianFacingPatientDashboardPage patientDashboardPage;
 
+
+    @Before
+    public void setUp() throws Exception {
+
+        
+        homePage = new HomePage(page);
+        assertPage(homePage);
+        headerPage = new HeaderPage(driver);
+        registrationPage = new RegistrationPage(driver);
+        patientDashboardPage = new ClinicianFacingPatientDashboardPage(page);
+
+    }
+
     @Test
-    @Category(BuildTests.class)
     public void editDemographicTest() throws Exception {
-        TestPatient patient = PatientGenerator.generateTestPatient();
+        homePage.goToActiveVisitPatient();
+        patientDashboardPage.clickOnEditPatient();
+        registrationPage.clearName();
+        registrationPage.enterPatientGivenName("John");
+        registrationPage.clearMiddleName();
+        registrationPage.enterPatientMiddleName("Bob");
+        registrationPage.clearFamilyName();
+        registrationPage.enterPatientFamilyName("Smith");
+        registrationPage.clickOnGenderLink();
+        registrationPage.selectPatientGender("Male");
+        registrationPage.clickOnBirthdateLabel();
+        registrationPage.clearBirthDay();
+        registrationPage.enterBirthDay("02");
+        registrationPage.selectBirthMonth("April");
+        registrationPage.clearBirthdateYear();
+        registrationPage.enterBirthYear("1985");
+        registrationPage.clickOnConfirmEdit();
+        registrationPage.confirmPatient();
+        assertTrue(driver.getPageSource().contains("Saved changes in contact info for: John Bob Smith"));
+    }
 
-        patientDashboardPage = homePage.goToActiveVisitPatient();
-        editDemographicsPage = patientDashboardPage.clickOnEditPatient();
+    @After
+    public void tearDown() throws Exception {
 
-        editDemographicsPage.enterPatientGivenName(patient.givenName);
-        editDemographicsPage.enterPatientMiddleName(patient.middleName);
-        editDemographicsPage.enterPatientFamilyName(patient.familyName);
-        editDemographicsPage.clickOnGenderLabel();
-        editDemographicsPage.selectPatientGender(patient.gender);
-        editDemographicsPage.clickOnBirthdateLabel();
-        editDemographicsPage.enterBirthDay(patient.birthDay);
-        editDemographicsPage.selectBirthMonth(patient.birthMonth);
-        editDemographicsPage.enterBirthYear(patient.birthYear);
-        editDemographicsPage.clickOnConfirmEdit();
-        editDemographicsPage.confirmPatient();
-
-        String[] names = patientDashboardPage.name().split(" ");
-
-        assertEquals(names[0], patient.givenName);
-        assertEquals(names[names.length - 1], patient.familyName);
-
-        assertEquals(patientDashboardPage.gender(), patient.gender);
-
-        String[] birthdate = patientDashboardPage.birthdate().split("\\.");
-
-        assertEquals(Integer.parseInt(birthdate[0]), Integer.parseInt(patient.birthDay));
-        assertTrue(patient.birthMonth.contains(birthdate[1]));
-        assertEquals(birthdate[2], patient.birthYear);
+        headerPage.clickOnHomeIcon();
+        headerPage.logOut();
     }
 
 
