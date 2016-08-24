f044e788bc40f597a1b6a5084fd8205bb9ecb506
Tomasz Marzeion <marzeion.tomasz@gmail.com>
2016-08-24 16:50:42 +0200

    RA-1120 - Fixed CaptureVitalsTest

--- ui-tests/src/test/java/org/openmrs/reference/CaptureVitalsTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/CaptureVitalsTest.java
@@ -1,42 +1,42 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.reference;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.openmrs.reference.groups.BuildTests;
+import org.openmrs.reference.page.ActiveVisitsPage;
 import org.openmrs.reference.page.ClinicianFacingPatientDashboardPage;
-import org.openmrs.reference.page.HomePage;
 import org.openmrs.reference.page.PatientCaptureVitalsPage;
-import org.openmrs.reference.page.PatientVisitsDashboardPage;
-import org.openmrs.uitestframework.test.TestBase;
-import org.openmrs.uitestframework.test.TestData.PatientInfo;
+import org.openmrs.uitestframework.test.RestClient;
+import org.openmrs.uitestframework.test.TestData;
 
 
-/**
- * Created by tomasz on 25.05.15.
- */
-public class CaptureVitalsTest  extends TestBase {
+public class CaptureVitalsTest  extends ReferenceApplicationTestBase {
 
+    private static final String VISIT_TYPE_UUID = "7b0f5697-27e3-40c4-8bae-f4049abfb4ed";
+    private static final String LOCATION_UUID = "8d6c993e-c2cc-11de-8d13-0010c6dffd0f";
 
-    private HomePage homePage;
-    private ClinicianFacingPatientDashboardPage clinicianFacingPatientDashboardPage;
+    private ActiveVisitsPage activeVisitsPage;
+    private ClinicianFacingPatientDashboardPage patientDashboardPage;
     private PatientCaptureVitalsPage patientCaptureVitalsPage;
-    private PatientInfo patient;
-    PatientVisitsDashboardPage patientVisitsDashboardPage;
+    private TestData.PatientInfo patient;
+
 
     @Before
-    public void setUp() throws InterruptedException {
-        homePage = new HomePage(page);
-        clinicianFacingPatientDashboardPage = new ClinicianFacingPatientDashboardPage(page);
-        patientVisitsDashboardPage = new PatientVisitsDashboardPage(driver);
-
-        patientCaptureVitalsPage = new PatientCaptureVitalsPage(driver);
-        assertPage(homePage);
-    }
-
-    private void registerAPatient() throws InterruptedException{
+    public void setup(){
         patient = createTestPatient();
-        clinicianFacingPatientDashboardPage.go(patient.uuid);
+        createTestVisit();
     }
 
     @After
@@ -45,12 +45,14 @@ public class CaptureVitalsTest  extends TestBase {
     }
 
     @Test
-    @Category(org.openmrs.reference.groups.BuildTests.class)
+    @Category(BuildTests.class)
     public void captureVital() throws InterruptedException {
-        registerAPatient();
-        clinicianFacingPatientDashboardPage.startVisit();
-        patientVisitsDashboardPage.goToCaptureVitals();
-        patientCaptureVitalsPage.waitForJsVariable("Navigator.isReady");
+
+        activeVisitsPage = homePage.goToActiveVisitsSearch();
+        activeVisitsPage.search(patient.identifier);
+
+        patientDashboardPage = activeVisitsPage.goToPatientDashboardOfLastActiveVisit();
+        patientCaptureVitalsPage = patientDashboardPage.goToPatientCaptureVitalsPage();
         patientCaptureVitalsPage.setHeightField("185");
         patientCaptureVitalsPage.setWeightField("78");
         patientCaptureVitalsPage.setTemperatureField("36.6");
@@ -62,5 +64,7 @@ public class CaptureVitalsTest  extends TestBase {
         patientCaptureVitalsPage.save();
     }
 
-
+    private void createTestVisit(){
+        JsonNode visit = RestClient.post("visit", new TestData.TestVisit(patient.uuid, VISIT_TYPE_UUID, LOCATION_UUID));
+    }
 }
