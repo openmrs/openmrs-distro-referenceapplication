3ce42f96bb8fbe22587b7e5a5816dde9a01fe0ef
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2016-08-29 11:02:39 +0200

    RA-1191: Fixing css selectors

--- ui-tests/src/main/java/org/openmrs/reference/page/FindPatientPage.java
+++ ui-tests/src/main/java/org/openmrs/reference/page/FindPatientPage.java
@@ -15,8 +15,8 @@ import org.openqa.selenium.WebDriver;
 
 public class FindPatientPage extends Page {
 
-    private static final By PATIENT_NAME_SEARCH_RESULT = By.cssSelector("#patient-search-results-table tr:first-child td:nth-child(2)");
-    private static final By PATIENT_ID_SEARCH_RESULT = By.cssSelector("#patient-search-results-table tr:first-child td:first-child");
+    private static final By PATIENT_NAME_SEARCH_RESULT = By.cssSelector("#patient-search-results-table tr:nth-child(2) td:nth-child(2)");
+    private static final By PATIENT_ID_SEARCH_RESULT = By.cssSelector("#patient-search-results-table tr:nth-child(2) td:first-child");
     private static final By PATIENT_SEARCH = By.id("patient-search");
 
     public FindPatientPage(WebDriver driver) {
