c7eaf58a9f832e27e9f977762b96fe746d5c521b
Rafal Korytkowski <rafal@openmrs.org>
2016-09-02 11:50:19 +0200

    Revert "RA-1208 - Fixed ContactInfoTest" (#160)

--- ui-tests/src/main/java/org/openmrs/reference/page/RegistrationEditSectionPage.java
+++ ui-tests/src/main/java/org/openmrs/reference/page/RegistrationEditSectionPage.java
@@ -10,10 +10,6 @@ import java.util.List;
 
 public class RegistrationEditSectionPage extends Page {
 
-    static final By CITY_VILLAGE = By.id("cityVillage");
-    static final By STATE_PROVINCE = By.id("stateProvince");
-    static final By COUNTRY = By.id("country");
-    static final By POSTAL_CODE = By.id("postalCode");
     static final By PHONE_NUMBER = By.name("phoneNumber");
     static final By CONFIRM_EDIT = By.xpath("//ul[@id='formBreadcrumb']/li[2]/span");
     static final By CONFIRM = By.cssSelector("input[value='Confirm']");
@@ -28,42 +24,9 @@ public class RegistrationEditSectionPage extends Page {
         return "/registrationapp/editSection.page";
     }
 
-    public void clearPhoneNumber(){
-        findElement(PHONE_NUMBER).clear();
-    }
+    public void clickOnPhoneNumberEdit(){clickOn(PHONE_NUMBER_EDIT);}
 
-    public void clearVillage(){
-        findElement(CITY_VILLAGE).clear();
-    }
-
-    public void clearState(){
-        findElement(STATE_PROVINCE).clear();
-    }
-
-    public void clearCountry(){
-        findElement(COUNTRY).clear();
-    }
-
-    public void clearPostalCode(){
-        findElement(POSTAL_CODE).clear();
-    }
-
-    public void clickOnPhoneNumberEdit(){
-        clickOn(PHONE_NUMBER_EDIT);
-    }
-
-    public void enterVillage(String familyName) {
-        setText(CITY_VILLAGE, familyName);
-    }
-    public void enterState(String familyName) {
-        setText(STATE_PROVINCE, familyName);
-    }
-    public void enterPostalCode(String familyName) {
-        setText(POSTAL_CODE, familyName);
-    }
-    public void enterCountry(String familyName) {
-        setText(COUNTRY, familyName);
-    }
+    public void clearPhoneNumber(){findElement(PHONE_NUMBER).clear();}
 
     public void clickOnConfirmEdit(){
         clickOn(CONFIRM_EDIT);
@@ -71,16 +34,6 @@ public class RegistrationEditSectionPage extends Page {
 
     public void enterPhoneNumber(String phone) {
         setText(PHONE_NUMBER, phone);
-    }
-
-    public String getInvalidPhoneNumberNotification() {
-        List<String> errors = getValidationErrors();
-        String search = "valid phone number";
-        for(String str: errors) {
-            if(str.trim().contains(search))
-                return str;
-        }
-        return null;
     }
 
     public ClinicianFacingPatientDashboardPage confirmPatient() throws InterruptedException{
