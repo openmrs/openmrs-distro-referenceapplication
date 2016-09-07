a275128369977239cf7a8a22eebad982d870b512
AdamGrzybkowski <agrzybkowski@outlook.com>
2016-09-07 14:05:37 +0200

    RA-1230 fix ServiceTest

--- ui-tests/src/main/java/org/openmrs/reference/page/ServicePage.java
+++ ui-tests/src/main/java/org/openmrs/reference/page/ServicePage.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.openmrs.uitestframework.page.Page;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 /**
@@ -17,138 +16,43 @@ public class ServicePage extends Page {
     public static final By DESCRIPTION_FIELD = By.id("description-field");
     public static final By SAVE_BUTTON = By.id("save-button");
     public static final By CANCEL_BUTTON = By.className("cancel");
-    public static final By NEW_SERVICE_TYPE = By.xpath("//button[@onclick=\"location.href='/openmrs/appointmentschedulingui/appointmentType.page'\"]");
-    public static final By APPOINTMENT_SCHEDULING = By.cssSelector("#appointmentschedulingui-homeAppLink-appointmentschedulingui-homeAppLink-extension > i.icon-calendar");
-    public static final By MANAGE_SERVICE_TYPES = By.id("appointmentschedulingui-manageAppointmentTypes-app");
-    public static final By CONFIRM = By.cssSelector("div.simplemodal-wrap > #delete-appointment-type-dialog > div.dialog-content > button.confirm.right");
-    public static final By EDIT_ICON = By.className("editAppointmentType");
-    public static final By DELETE_ICON = By.className("deleteAppointmentType");
-    public static final String CURRENT_SERVICE_TYPE_DELETE = "appointmentschedulingui-delete-";
-    public static final String CURRENT_SERVICE_TYPE_EDIT = "appointmentschedulingui-edit-";
-    public static final By ERROR_MESSAGE = By.xpath("//table[@id='appointmentTypesTable']/tbody/tr/td");
-    public ServicePage(WebDriver driver) {
-        super(driver);
-    }
 
     @Override
     public String getPageUrl() {
         return URL_PATH;
     }
 
-    public void putServiceData(String name, String duration, String description) {
-        putName(name);
-        putDuration(duration);
-        putDescription(description);
-        save();
+    public ServicePage(Page parent) {
+        super(parent);
     }
 
-
-    private void putName(String name) {
-        findElement(NAME_FIELD).clear();
-        findElement(NAME_FIELD).sendKeys(name);
+    public void setName(String name) {
+        setTextToFieldNoEnter(NAME_FIELD, name);
     }
 
-    public void editServiceName(String name) {
-        putName(name);
-        save();
+    public void setDuration(String duration) {
+        setTextToFieldNoEnter(DURATION_FIELD, duration);
     }
 
-
-    private void putDuration(String duration) {
-        findElement(DURATION_FIELD).clear();
-        findElement(DURATION_FIELD).sendKeys(duration);
+    public void setDescription(String description) {
+        setTextToFieldNoEnter(DESCRIPTION_FIELD, description);
     }
 
-    public void editServiceDuration(String duration) {
-        putDuration(duration);
-        save();
-    }
-
-    private void putDescription(String description) {
-        findElement(DESCRIPTION_FIELD).clear();
-        findElement(DESCRIPTION_FIELD).sendKeys(description);
-    }
-
-    private void save() {
-        findElement(SAVE_BUTTON).click();
+    public ManageServiceTypesPage save() {
+        clickOn(SAVE_BUTTON);
+        return new ManageServiceTypesPage(this);
     }
 
     public void cancel() {
-        findElement(CANCEL_BUTTON).click();
+        clickOn(CANCEL_BUTTON);
     }
 
-    public void openManageServiceTypes()
-    {
-        findElement(APPOINTMENT_SCHEDULING).click();
-        findElement(MANAGE_SERVICE_TYPES).click();
-    }
-
-    public void addNewService() throws InterruptedException{
-        waitForServiceMenu();
-        clickOn(NEW_SERVICE_TYPE);
-        waitForServicePage();
-    }
-
-    public void waitForServiceMenu() {
-        waitForElement(NEW_SERVICE_TYPE);
-    }
-
-    public void waitForServicePage() {
-        waitForElement(DESCRIPTION_FIELD);
-    }
-
-//    public boolean serviceExists(String service) {
-//        int i =1;
-//        try {
-//            while (!serviceExistsOnPage(service)) {
-//                i++;
-//                findElement(By.linkText(i + "")).click();
-//                Thread.sleep(100);
-//            }
-//        } catch(Exception e) {
-//            return false;
-//        }
-//        return true;
-//    }
-
-    public boolean serviceExistsOnPage(String service) {
-        try {
-            if (findElement(By.id(CURRENT_SERVICE_TYPE_DELETE + service)) != null) {
-                return true;
-            }
-            return false;
-        } catch(Exception e) {
-            return false;
-        }
-    }
-
-    public List<WebElement> getElementsFromDeleted() {
-        return findElement(By.xpath("//table[@id='appointmentTypesTable']/tbody/tr[1]")).findElements(By.tagName("td"));
-    }
-
-    public void deleteService(String service) {
-        findElement(By.id(CURRENT_SERVICE_TYPE_DELETE + service)).click();
-        confirmDelete();
-    }
-
-    public void startEditService(String service) {
-        findElement(By.id(CURRENT_SERVICE_TYPE_EDIT + service)).click();
-    }
-
-    public String getValue(By field) {
+    private String getValue(By field) {
         String text = getText(field);
         if(!text.isEmpty()) {
             return text;
         }
         return findElement(field).getAttribute("value");
-    }
-
-    public String getValue(WebElement element) {
-        String text = element.getText();
-        if(!text.isEmpty()) {
-            return text;
-        }
-        return element.getAttribute("value");
     }
 
     public String getNameValue() {
@@ -161,18 +65,6 @@ public class ServicePage extends Page {
 
     public String getDescriptionValue() {
         return getValue(DESCRIPTION_FIELD);
-    }
-
-    public void clickOnEdit() {
-        findElement(EDIT_ICON).click();
-    }
-
-    public void clickOnDelete() {
-        findElement(DELETE_ICON).click();
-    }
-
-    public void confirmDelete() {
-        findElement(CONFIRM).click();
     }
 
 }
---
+++ ui-tests/src/test/java/org/openmrs/reference/EditServiceTest.java
@@ -0,0 +1,52 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.reference;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.reference.page.AppointmentSchedulingPage;
+import org.openmrs.reference.page.ManageServiceTypesPage;
+import org.openmrs.reference.page.ServicePage;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class EditServiceTest extends ReferenceApplicationTestBase{
+
+    private String name;
+    private String duration;
+    private String description;
+
+    @Before
+    public void setup() {
+        name = RandomStringUtils.randomAlphabetic(5);
+        duration = RandomStringUtils.randomNumeric(2);
+        description = RandomStringUtils.randomAlphabetic(10);
+    }
+
+    @Test
+    public void editServiceTest() throws InterruptedException{
+        AppointmentSchedulingPage appointmentSchedulingPage = homePage.goToAppointmentScheduling();
+        ManageServiceTypesPage manageServiceTypesPage = appointmentSchedulingPage.goToManageServices();
+        ServicePage servicePage = manageServiceTypesPage.clickOnNewServiceType();
+        servicePage.setName(name);
+        servicePage.setDuration(duration);
+        servicePage.setDescription(description);
+        manageServiceTypesPage = servicePage.save();
+        assertThat(manageServiceTypesPage.getSertviceType(name), is(true));
+        servicePage = manageServiceTypesPage.editServiceType(name);
+        servicePage.setName(name+name);
+        manageServiceTypesPage = servicePage.save();
+        assertThat(manageServiceTypesPage.getSertviceType(name+name), is(true));
+        manageServiceTypesPage.deleteServiceType(name+name);
+        manageServiceTypesPage.confirmDelete();
+    }
+}
