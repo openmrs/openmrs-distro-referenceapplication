11a9392f2b2395725a0a7d19874c08ac8420c0e1
AdamGrzybkowski <agrzybkowski@outlook.com>
2016-09-08 14:28:41 +0200

    RA-1220 fixed location and visit  uuid"

--- ui-tests/src/test/java/org/openmrs/reference/MergeVisitsTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/MergeVisitsTest.java
@@ -25,15 +25,12 @@ import static org.hamcrest.Matchers.is;
 
 public class MergeVisitsTest extends ReferenceApplicationTestBase {
 
-    private static final String VISIT_TYPE_UUID = "7b0f5697-27e3-40c4-8bae-f4049abfb4ed";
-    private static final String LOCATION_UUID = "8d6c993e-c2cc-11de-8d13-0010c6dffd0f";
-
     private TestData.PatientInfo patient;
 
     @Before
     public void setUp() {
         patient = createTestPatient();
-        new TestData.TestVisit(patient.uuid, VISIT_TYPE_UUID, LOCATION_UUID).create();
+        new TestData.TestVisit(patient.uuid, TestData.getAVisitType(), getLocationUuid(homePage)).create();
     }
 
     @Test
