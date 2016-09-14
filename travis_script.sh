39ed4e1974ad8209f4fab6f0e688c519397244f0
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2016-09-14 16:26:37 +0200

    Moved travis_wait to correct place

--- travis_script.sh
+++ travis_script.sh
@@ -3,7 +3,7 @@
 # Run UI tests only, if the build was triggered by API from Bamboo, see travis_trigger.sh
 if [ "$TRAVIS_EVENT_TYPE" = "api" ]
 then
-travis_wait 50 mvn clean verify -Pci -Dwebapp.url=http://localhost:8080/openmrs -Dsaucelabs.hub.url=localhost:4445 -DbuildNumber=$TRAVIS_BUILD_NUMBER -Dtunnel.identifier=$TRAVIS_JOB_NUMBER
+mvn clean verify -Pci -Dwebapp.url=http://localhost:8080/openmrs -Dsaucelabs.hub.url=localhost:4445 -DbuildNumber=$TRAVIS_BUILD_NUMBER -Dtunnel.identifier=$TRAVIS_JOB_NUMBER
 else
 mvn clean install -DskipTests=true
 fi
