3f90586503f1d1e6465b2e113028f44bb96f87be
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2016-09-15 11:42:59 +0200

    Adding custom TRAVIS_MESSAGE

--- travis_trigger.sh
+++ travis_trigger.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Triggers a build of https://travis-ci.org/openmrs/openmrs-distro-referenceapplication
-# In order to use the script you have to set the TRAVIS_ACCESS_TOKEN environment variable.
+# In order to use the script you have to set the TRAVIS_ACCESS_TOKEN and TRAVIS_MESSAGE environment variable.
 
 curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
   -H "Travis-API-Version: 3" \
   -H "Authorization: token $TRAVIS_ACCESS_TOKEN" \
-  -d '{ "request": { "branch": "master", "message": "Running UI Tests" } }' \
+  -d '{ "request": { "branch": "master", "message": "$TRAVIS_MESSAGE" } }' \
   https://api.travis-ci.org/repo/openmrs%2Fopenmrs-distro-referenceapplication/requests
