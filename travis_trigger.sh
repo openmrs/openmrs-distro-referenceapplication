2f9413a1aeebabfe465d4e94fbcba31b46826ff3
Rafal Korytkowski <rafal@openmrs.org>
2016-09-19 14:48:01 +0200

    Disabling platform-2.1.x for now

--- travis_trigger.sh
+++ travis_trigger.sh
@@ -11,10 +11,10 @@ curl -s -X POST \
   -d "{ \"request\": { \"branch\": \"master\", \"message\": \"$TRAVIS_MESSAGE\" } }" \
   https://api.travis-ci.org/repo/openmrs%2Fopenmrs-distro-referenceapplication/requests
 
-curl -s -X POST \
-  -H "Content-Type: application/json" \
-  -H "Accept: application/json" \
-  -H "Travis-API-Version: 3" \
-  -H "Authorization: token $TRAVIS_ACCESS_TOKEN" \
-  -d "{ \"request\": { \"branch\": \"platform-2.1.x\", \"message\": \"$TRAVIS_MESSAGE\" } }" \
-  https://api.travis-ci.org/repo/openmrs%2Fopenmrs-distro-referenceapplication/requests
+#curl -s -X POST \
+#  -H "Content-Type: application/json" \
+#  -H "Accept: application/json" \
+#  -H "Travis-API-Version: 3" \
+#  -H "Authorization: token $TRAVIS_ACCESS_TOKEN" \
+#  -d "{ \"request\": { \"branch\": \"platform-2.1.x\", \"message\": \"$TRAVIS_MESSAGE\" } }" \
+#  https://api.travis-ci.org/repo/openmrs%2Fopenmrs-distro-referenceapplication/requests
