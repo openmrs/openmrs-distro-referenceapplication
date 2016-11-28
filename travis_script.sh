1585085840d53eb48f1eda4d13c23211ffd080d2
Rafal Korytkowski <rafal@openmrs.org>
2016-11-28 16:26:11 +0100

    Update travis_script.sh

--- travis_script.sh
+++ travis_script.sh
@@ -2,7 +2,7 @@
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-mvn clean verify -Pci -Dwebapp.url=http://localhost:8080/openmrs -Dsaucelabs.hub.url=localhost:4445 -DbuildNumber=$TRAVIS_BUILD_NUMBER -Dbranch=$TRAVIS_BRANCH -DsaucelabsTunnel=$TRAVIS_JOB_NUMBER
+mvn clean verify -Pci -Dwebapp.url=http://localhost:8080/openmrs -Dsaucelabs.hub.url=localhost:4445 -DbuildNumber=$TRAVIS_BUILD_NUMBER -Dbranch=$TRAVIS_BRANCH -DsaucelabsTunnel=$TRAVIS_JOB_NUMBER -Dmaven.junit.usefile=false
 else
 mvn clean install -DskipTests=true
 fi
