63e8f0cc21710589875e7a72ab3a6d80b6bbb63d
Rafal Korytkowski <rafal@openmrs.org>
2017-01-24 16:09:56 +0100

    Adjusted to the latest version of SDK

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -2,10 +2,9 @@
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-mvn org.openmrs.maven.plugins:openmrs-sdk-maven-plugin:setup-sdk -DbatchAnswers=n
-mvn org.openmrs.maven.plugins:openmrs-sdk-maven-plugin:build-distro -Ddir=distro -e
+mvn org.openmrs.maven.plugins:openmrs-sdk-maven-plugin:build-distro -Ddir=distro -e -B
 cd distro
-docker-compose up -d #detached mode
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d #detached mode
 cd ..
 sleep 60
 fi
