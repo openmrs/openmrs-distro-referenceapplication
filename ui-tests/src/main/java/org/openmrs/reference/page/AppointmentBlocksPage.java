55824f39cd04b4540ca3493190b4655222e0b540
DomenicoDiLeo <domenico.dileo@gmail.com>
2017-03-04 17:44:48 +0100

    RA-904: tests for ManageProviderSchedule have been enabled and significantly refactored

--- ui-tests/src/main/java/org/openmrs/reference/page/AppointmentBlocksPage.java
+++ ui-tests/src/main/java/org/openmrs/reference/page/AppointmentBlocksPage.java
@@ -10,9 +10,13 @@
 
 package org.openmrs.reference.page;
 
+
+import java.util.List;
+
 import org.openmrs.uitestframework.page.Page;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class AppointmentBlocksPage extends Page {
 
@@ -21,25 +25,42 @@ public class AppointmentBlocksPage extends Page {
     private static final By LOCATION = By.className("ng-pristine");
     private static final By START_TIME = By.xpath("(//input[@type='text'])[5]");
     private static final By SERVICE = By.id("createAppointmentBlock");
-    private static final By BLOCK = By.className("fc-event-title");
     private static final By SERVICE_DROPDOWN = By.cssSelector("a.ng-scope.ng-binding");
     private static final By EDIT_BLOCK= By.linkText("Edit");
     private static final By SAVE = By.cssSelector("button.confirm");
     private static final By PROVIDER = By.xpath("(//input[@type='text'])[3]");
-    public static final By CURRENT_DAY = By.className("fc-state-highlight");
+    //This identifier works if the calendar shows only the day 
+    private static final By SERVICE_TITLE_OF_THE_DAY = By.className("fc-event-title");
+    private	static final By DAY = By.xpath("/html/body/div/div[3]/div[2]/div[1]/div[2]/table/tbody/tr/td[1]/span[5]");
+    private static final By DAY_CLASS = By.className("fc-day-content");
+    public static final By CURRENT_DAY = By.className("fc-button fc-button-today fc-state-default fc-corner-left fc-corner-right fc-state-disabled");
     private static final By LOCATION_IN_BLOCK = By.xpath("//div[@id='select-location']/select");
     public static final By DELETE = By.linkText("Delete");
     private static final By DELETE_CONFIRM = By.cssSelector("#delete-appointment-block-modal-buttons .confirm");
     private static final By SERVICE_DELETE = By.xpath("//div[@id='appointment-block-form']/selectmultipleappointmenttypes/div/div/div/div/i");
     private static final By CLOSE_WINDOW = By.cssSelector("#delete-appointment-block-modal > div.dialog-header > h3");
-    private static final By COUNT_BLOCK = By.xpath("//div[@id='calendar']/div/div/div/div");
+    private static final By SERVICE_BLOCK = By.className("fc-event-inner");
+    private static final By DAY_BLOCK= By.xpath("//table[@class='fc-border-separate']/tbody/tr/td/div");
+    private static final By REMOVE_APPOINTMENT = By.xpath("//div[@id='appointment-block-form']/selectmultipleappointmenttypes/div/div/div/div/i");
+    private static final By CANCEL = By.className("cancel");
+    
     public void goToAppointmentBlock(){
         clickOn(APPOINTMENT_SCHEDULING);
         clickOn(MANAGE_PROVIDER_SCHEDULES);
     }
 
+    /**
+     * This method removes an appointment.  
+     * Before calling this method is necessary to 
+     * click on the day (There should be only one block on the page)
+     */
+    public void removeAppointment(){
+    	clickOn(REMOVE_APPOINTMENT);
+    }
     public void selectLocation(String location) {
-        selectFrom(LOCATION, location);
+    	// a wait might be needed if there are many elements to load in the dropdown menu
+        waiter.until(ExpectedConditions.elementToBeClickable(LOCATION));
+    	selectFrom(LOCATION, location);
         clickOn(LOCATION);
     }
 
@@ -70,6 +91,39 @@ public class AppointmentBlocksPage extends Page {
     public void clickOnCurrentDay() throws InterruptedException{clickOn(CURRENT_DAY);}
     public void enterStartTime(String start){setTextToFieldNoEnter(START_TIME, start);}
 
+    /**
+     * This method scrolls the page up 
+     * This method has been created since the delete button 
+     * is hidden by the tooltip. 
+     * An alternative solution to this method is to create a method 
+     * that close the tooltip such as:
+     * private static final By CLOSE_TOOLTIP = By.cssSelector("span.ui-icon.ui-icon-close");
+     * public void clickOnCloseTooltip(){
+     *	clickOn(CLOSE_TOOLTIP);
+     *	}
+     *Unfortunately this method did not prove to be always reliable (sometime Selenium fails to locate span.ui-icon.ui-icon-close) 
+     */
+    public void clickOnleft(){
+    	executeScript("scroll(300, 0)");
+    }
+    
+    /**
+     * This method clicks on the current day and open the "editing page"
+     * 
+     */
+    public void clickOnDay(){
+    	clickOn(DAY);
+    	clickOn(DAY_CLASS);
+    }
+    
+    /**
+     *This method clicks on the Cancel button available in the "editing page" 
+     * 
+     **/
+    public void clickOnCancel(){
+    	clickOn(CANCEL);
+    }
+        
     public void clickOnSave(){
         clickOn(SAVE);
         try {
@@ -78,6 +132,13 @@ public class AppointmentBlocksPage extends Page {
         {
 
         }
+    }
+    /**
+     *Checks if the the Save button available in the "editing page" is enabled 
+     */
+    
+    public Boolean isSaveEnabled(){
+    	return findElement(SAVE).isEnabled();
     }
 
     public void clickOnDelete() throws InterruptedException{
@@ -88,24 +149,41 @@ public class AppointmentBlocksPage extends Page {
     public void clickOnEdit(){clickOn(EDIT_BLOCK);}
     public void clickOnServiceDelete(){clickOn(SERVICE_DELETE);}
     public AppointmentBlocksPage(Page page) {super(page);}
-
-
-    public void findBlock() {
-
-        try {
-            findElement(By.xpath("//div[@id='calendar']/div/div/div/div[" + driver.findElements(COUNT_BLOCK).size() + "]/div/span[2]")).click();
-        } catch (Exception e) {
-
-        }
+    
+    public void clickOnAppointment(){
+    	findElement(SERVICE_BLOCK).click();;
     }
+    
+    /**
+     * This method clicks on the Cancel button
+     */
     public void clickOnClose(){
         findElement(CLOSE_WINDOW);
         clickOn(CLOSE_WINDOW);
     }
+    
+    /**
+     * This methods extracts the service from a row displayed in the block
+     * 
+     * @param serviceChoice, the index of the service. For instance if there are 2 rows on the block, to get the first one searchChoice = 0
+     * @return the Service in the row  (e.g. Dermatology)
+     */
+    public String getServiceOfDay(int serviceChoice){
 
-
-
-
+    		List<WebElement> elements = findElements(SERVICE_TITLE_OF_THE_DAY);
+	    	if(null != elements && elements.size() >= serviceChoice)
+	    	{
+	    		return elements.get(serviceChoice).getText();
+	    	}
+	    	else{
+	    		return new String();
+	    	}
+    }
+    
+    public void clickOnDayBlock(){
+    	clickOn(DAY_BLOCK);
+    }
+    
     @Override
     public String getPageUrl() {
         return "/appointmentschedulingui/scheduleProviders.page";
--- ui-tests/src/test/java/org/openmrs/reference/DeleteAppointmentBlockTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/DeleteAppointmentBlockTest.java
@@ -1,53 +1,59 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 
 
 package org.openmrs.reference;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.openmrs.reference.page.*;
-import org.openmrs.uitestframework.test.TestBase;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-
-/**
- * Created by nata on 08.07.2015.
- */
-public class DeleteAppointmentBlockTest extends TestBase {
-    private HomePage homePage;
-    private HeaderPage headerPage;
-    private AppointmentBlocksPage appointmentBlocksPage;
-
+public class DeleteAppointmentBlockTest extends ManageProviderSchedulesTest {
+    
+	private String correctStartTimeFirtAppointment = "08";
+    private String outboundStartTime = "30";
+    private String firstAppointment = "Gynecology";
+    int firstAppointmentIndex = 0;
+    
+    
     @Before
     public void setUp() throws Exception {
-        
-        homePage = new HomePage(page);
-        assertPage(homePage);
-        headerPage = new HeaderPage(driver);
-        appointmentBlocksPage = new AppointmentBlocksPage(page);
+    	super.setUp();
     }
-
-    @Ignore//ignored due to possible application logout
+	
     @Test
     public void deleteAppointmentBlockTest() throws Exception {
-        appointmentBlocksPage.goToAppointmentBlock();
-        appointmentBlocksPage.selectLocation("Outpatient Clinic");
-        appointmentBlocksPage.clickOnCurrentDay();
-        appointmentBlocksPage.selectLocationBlock("Outpatient Clinic");
-        appointmentBlocksPage.enterService("derm");
-        appointmentBlocksPage.enterProvider("Super User");
+    	
+    	//The logic behind the text is to create an appointment and delete it
+    	appointmentBlocksPage.selectLocation(locationName);
+        appointmentBlocksPage.clickOnDay();
+        //Here we make negative test: using outbound values for start time should result 
+        // that Save button is disabled
+        
+        appointmentBlocksPage.enterStartTime(outboundStartTime);
+        assertTrue(!appointmentBlocksPage.isSaveEnabled());
+        appointmentBlocksPage.enterStartTime(correctStartTimeFirtAppointment);
+        appointmentBlocksPage.enterService("gyne");
+        appointmentBlocksPage.enterProvider(provider);
         appointmentBlocksPage.clickOnSave();
-        assertNotNull("Dermatology", appointmentBlocksPage.CURRENT_DAY);
-        appointmentBlocksPage.findBlock();
+        assertTrue(appointmentBlocksPage.getServiceOfDay(firstAppointmentIndex).equals(firstAppointment));
+        //let'remove the appointment
+        appointmentBlocksPage.clickOnAppointment();
         appointmentBlocksPage.clickOnDelete();
-        appointmentBlocksPage.clickOnClose();
+        appointmentBlocksPage.clickOnleft();
         appointmentBlocksPage.clickOnConfirmDelete();
     }
     @After
     public void tearDown() throws Exception {
-        headerPage.clickOnHomeIcon();
-        headerPage.logOut();
+    	super.tearDown();
     }
 }
 
