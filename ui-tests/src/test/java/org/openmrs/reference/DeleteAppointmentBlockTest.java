c68741238abe804347a9d2f2342fc09feca65b76
dkayiwa <dkayiwa@openmrs.org>
2017-03-13 00:32:16 +0300

    Revert "RA-1320: Tests for Manage Provider Schedule refactored and enabled (#214)"
    This reverts commit 97b7e59cdd7e74b7967c911dd37f409997253487.

--- ui-tests/src/test/java/org/openmrs/reference/DeleteAppointmentBlockTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/DeleteAppointmentBlockTest.java
@@ -1,64 +1,53 @@
-/**
- * This Source Code Form is subject to the terms of the Mozilla Public License,
- * v. 2.0. If a copy of the MPL was not distributed with this file, You can
- * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
- * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- *
- * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
- * graphic logo is a trademark of OpenMRS Inc.
- */
+
 
 package org.openmrs.reference;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.openmrs.reference.page.*;
+import org.openmrs.uitestframework.test.TestBase;
+import static org.junit.Assert.assertNotNull;
 
-public class DeleteAppointmentBlockTest extends ManageProviderSchedulesTest {
-	
-	private String correctStartTimeFirtAppointment = "08";
-	
-	private String outboundStartTime = "30";
-	
-	private String firstAppointment = "Gynecology";
-	
-	int firstAppointmentIndex = 0;
-	
-	@Before
-	public void setUp() throws Exception {
-		super.setUp();
-	}
-	
-	/*
-	 * @verifies creation and delete of an appointment
-	 */
-	@Test
-	public void deleteAppointmentBlockTest() throws Exception {
-		
-		//The logic behind the text is to create an appointment and delete it
-		appointmentBlocksPage.selectLocation(locationName);
-		appointmentBlocksPage.clickOnDay();
-		/*
-		 * Here we make negative test: using outbound values for start time should result
-		 * that Save button is disabled 
-		 */
-		appointmentBlocksPage.enterStartTime(outboundStartTime);
-		assertTrue(!appointmentBlocksPage.isSaveEnabled());
-		appointmentBlocksPage.enterStartTime(correctStartTimeFirtAppointment);
-		appointmentBlocksPage.enterService("gyne");
-		appointmentBlocksPage.enterProvider(provider);
-		appointmentBlocksPage.clickOnSave();
-		assertTrue(appointmentBlocksPage.getServiceOfDay(firstAppointmentIndex).equals(firstAppointment));
-		//let'remove the appointment
-		appointmentBlocksPage.clickOnAppointment();
-		appointmentBlocksPage.clickOnDelete();
-		appointmentBlocksPage.clickOnleft();
-		appointmentBlocksPage.clickOnConfirmDelete();
-	}
-	
-	@After
-	public void tearDown() throws Exception {
-		super.tearDown();
-	}
+
+/**
+ * Created by nata on 08.07.2015.
+ */
+public class DeleteAppointmentBlockTest extends TestBase {
+    private HomePage homePage;
+    private HeaderPage headerPage;
+    private AppointmentBlocksPage appointmentBlocksPage;
+
+    @Before
+    public void setUp() throws Exception {
+        
+        homePage = new HomePage(page);
+        assertPage(homePage);
+        headerPage = new HeaderPage(driver);
+        appointmentBlocksPage = new AppointmentBlocksPage(page);
+    }
+
+    @Ignore//ignored due to possible application logout
+    @Test
+    public void deleteAppointmentBlockTest() throws Exception {
+        appointmentBlocksPage.goToAppointmentBlock();
+        appointmentBlocksPage.selectLocation("Outpatient Clinic");
+        appointmentBlocksPage.clickOnCurrentDay();
+        appointmentBlocksPage.selectLocationBlock("Outpatient Clinic");
+        appointmentBlocksPage.enterService("derm");
+        appointmentBlocksPage.enterProvider("Super User");
+        appointmentBlocksPage.clickOnSave();
+        assertNotNull("Dermatology", appointmentBlocksPage.CURRENT_DAY);
+        appointmentBlocksPage.findBlock();
+        appointmentBlocksPage.clickOnDelete();
+        appointmentBlocksPage.clickOnClose();
+        appointmentBlocksPage.clickOnConfirmDelete();
+    }
+    @After
+    public void tearDown() throws Exception {
+        headerPage.clickOnHomeIcon();
+        headerPage.logOut();
+    }
 }
+
