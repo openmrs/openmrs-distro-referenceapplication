5a9651db8ed0a52ff7d13cc71d7702a382e0189c
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2017-07-27 15:41:41 +0200

    Changed test to verify

--- travis_script.sh
+++ travis_script.sh
@@ -20,7 +20,7 @@ fi
 sleep 60
 
 cd ../../../ui-tests
-mvn test -Pci -Dwebapp.url=http://localhost:8080/openmrs -Dsaucelabs.hub.url=localhost:4445 -DbuildNumber=$TRAVIS_BUILD_NUMBER -Dbranch=$TRAVIS_BRANCH -DsaucelabsTunnel=$TRAVIS_JOB_NUMBER -Dmaven.junit.usefile=false
+mvn verify -Pci -Dwebapp.url=http://localhost:8080/openmrs -Dsaucelabs.hub.url=localhost:4445 -DbuildNumber=$TRAVIS_BUILD_NUMBER -Dbranch=$TRAVIS_BRANCH -DsaucelabsTunnel=$TRAVIS_JOB_NUMBER -Dmaven.junit.usefile=false
 
 else
 mvn clean install -U
