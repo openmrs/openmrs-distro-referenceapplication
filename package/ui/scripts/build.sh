4747fae844fa78205f7ebebc2c54878c5f75feec
rbuisson <romain@mekomsolutions.com>
2021-02-16 20:01:05 +0100

    MF-479: Add missing ESMs to the distribution

--- package/ui/scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-#npx openmrs build --spa-path \${SPA_PATH} --api-url \${API_URL} --target ./target/microfrontends
-npx openmrs build --spa-path /ui --api-url /openmrs --target $1
