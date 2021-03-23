48acda00084b6c64f4da9d40ffa29ef58e2278e6
k-joseph <kaweesi@jembi.org>
2021-03-23 14:03:04 +0300

    RATEST-110: moved running ui-tests from travis to githubActions

--- travis_script.sh
+++ travis_script.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 mvn org.openmrs.maven.plugins:openmrs-sdk-maven-plugin:3.9.0:setup-sdk -DbatchAnswers=n
-mvn clean install -DskipTests=false -U -q
+mvn clean install -U -q
 cd package/target/distro
 export TOMCAT_PORT=8080
 
@@ -23,5 +23,5 @@ cd ../../../ui-tests
 mvn verify -Pci -Dwebapp.url=http://localhost:8080/openmrs -Dsaucelabs.hub.url=localhost:4445 -DbuildNumber=$TRAVIS_BUILD_NUMBER -Dbranch=$TRAVIS_BRANCH -DsaucelabsTunnel=$TRAVIS_JOB_NUMBER -Dmaven.junit.usefile=false
 
 else
-mvn clean install -DskipTests=false -U -q
+mvn clean install -U -q
 fi
