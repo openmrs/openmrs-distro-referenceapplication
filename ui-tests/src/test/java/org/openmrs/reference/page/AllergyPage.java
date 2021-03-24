6164c3efce87ebf282e2c33af18e564f45c92549
achilep <pahonsibebeto@gmail.com>
2021-03-24 22:21:08 +0100

    RATEST-78 Allergies automated tests

--- ui-tests/src/test/java/org/openmrs/reference/page/AllergyPage.java
+++ ui-tests/src/test/java/org/openmrs/reference/page/AllergyPage.java
@@ -12,6 +12,7 @@ package org.openmrs.reference.page;
 
 import org.openmrs.uitestframework.page.Page;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class AllergyPage extends Page {
 
@@ -25,6 +26,15 @@ public class AllergyPage extends Page {
     private static final By ADD_NO_KNOWN = By
             .xpath("//*[contains(text(),'No Known Allergy')]");
     private static final By DELETE_NO_KNOWN = By.className("delete-action");
+    public static final By CHOOSEDRUG = By.id("allergen-162299");
+    public static final By CHOOSEREACTION = By.id("reaction-121629");
+    public static final By SELECTSEVERITY = By.id("severity-1498");
+    public static final By COMMENT = By.id("allergy-comment");
+    public static final By SAVE = By.id("addAllergyBtn");
+    public static final By CANCEL = By.className("cancel");
+    public static final By ALLERGYTYPE = By.id("types");
+    public static final By AllERGEN = By.id("allergens");
+    public static final By SEVERTIES = By.name("severity");
 
     public AllergyPage(Page page) {
         super(page);
@@ -67,6 +77,68 @@ public class AllergyPage extends Page {
     public void removeNoKnownAllergy() {
         driver.findElement(DELETE_NO_KNOWN).click();
     }
+
+    public  void clickOnAddNewAllergy(){
+        driver.findElement(ADD_NEW_ALLERGY).click();
+    }
+
+    public void chooseDrug(){
+        driver.findElement(CHOOSEDRUG).click();
+    }
+
+    public void chooseReaction(){
+        driver.findElement(CHOOSEREACTION).click();
+    }
+
+    public void selectSeverity(){
+        driver.findElement(SELECTSEVERITY).click();
+
+    public void allergyComment(){
+        setText(COMMENT,"I have malaria");
+    }
+
+    public void clickOnSaveButon(){
+        driver.findElement(SAVE).click();
+    }
+
+    public void clickOnCancelButon(){
+        driver.findElement(CANCEL).click();
+    }
+
+
+    public void clickOnEdditAllergy(){
+        driver.findElement(EDIT_ALLERGY).click();
+    }
+
+    public void selectAllergyType(int allergytype){
+        driver.findElement(ALLERGYTYPE).getTagName("label")[allergytype].click();
+    }
+
+    public String getSeclectedAllergernclass(int allergytype){
+        WebElement l = driver.findElement(AllERGEN).getTagName("ul")[allergytype];
+        return l.getAttribute('class');
+    }
+
+    public void selectalleregn(int allergytype){
+        WebElement l = driver.findElement(AllERGEN).getTagName("ul")[allergytype];
+        int allergen = (int)(Math.random()*(3) + 0);
+        l.getTagName('li')[allergen];
+        l.isSelected();
+    }
+
+    public void selectReaction(){
+        WebElement l = driver.findElement(REACTION).getTagName("li")[5];
+        l.isSelected();
+    }
+
+    public void Selectseverity(int severity){
+        WebElement l = driver.findElement(SEVERTIES)[severity];
+        l.isSelected();
+    }
+    public void addComment(String comment){
+        driver.findElement(COMMENT).sendKeys(comment);
+    }
+
 
     @Override
     public String getPageUrl() {
