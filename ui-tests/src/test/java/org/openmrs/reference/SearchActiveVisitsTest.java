302d2546ee909c738ebb8f37ad216bc4f4706b75
sherrif10 <sharfspace@gmail.com>
2021-04-06 09:47:02 +0300

    RATEST-115:Un Ignore SearchActiveVisitTest

--- ui-tests/src/test/java/org/openmrs/reference/SearchActiveVisitsTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/SearchActiveVisitsTest.java
@@ -11,7 +11,6 @@ package org.openmrs.reference;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.experimental.categories.Category;
 import org.openmrs.reference.groups.BuildTests;
 import org.openmrs.reference.page.ActiveVisitsPage;
@@ -31,7 +30,7 @@ public class SearchActiveVisitsTest extends ReferenceApplicationTestBase {
         new TestData.TestVisit(patient.uuid, TestData.getAVisitType(), getLocationUuid(homePage)).create();
     }
 
-    @Ignore
+ 
     @Category(BuildTests.class)
     public void searchActiveVisitsByPatientNameOrIdOrLastSeenTest() throws Exception {
         ActiveVisitsPage activeVisitsPage = homePage.goToActiveVisitsSearch();
