85eaaff151ecf3277cac1f7d1d1957fafe06408c
sherrif10 <sharfspace@gmail.com>
2021-04-26 08:56:21 +0300

    RATEST-93:Failing Test:Delete Form Test

--- ui-tests/src/test/java/org/openmrs/reference/DeleteFormTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/DeleteFormTest.java
@@ -4,10 +4,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.openmrs.reference.page.AdministrationPage;
 import org.openmrs.reference.page.HeaderPage;
 import org.openmrs.reference.page.HomePage;
 import org.openmrs.reference.page.ManageFormsPage;
 import org.openmrs.uitestframework.test.TestBase;
+import org.openqa.selenium.By;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -19,19 +21,33 @@ public class DeleteFormTest extends TestBase {
     private HomePage homePage;
     private HeaderPage headerPage;
     private ManageFormsPage manageForm;
-
+    private static String name = "newFormTest";
+    private static String description = "description of new form";
+    private static  String version = "1.2";
+    private static final By NEW_HTML_FORM = By.cssSelector("a[href='htmlForm.form']");
+    private AdministrationPage administrationPage;
+    //    private ManageHtmlFormsPage manageHtmlFormsPage;
+//    private HtmlFormsPage htmlFormsPage;
     @Before
     public void setUp() throws Exception {
 
         homePage = new HomePage(page);
-        assertPage(homePage);
+//        assertPage(homePage);
+        administrationPage = new AdministrationPage(page);
         headerPage = new HeaderPage(driver);
         manageForm = new ManageFormsPage(driver);
     }
 
-    @Ignore//ignore due to moving forms functionality
     @Test
     public void deleteFormTest() throws Exception {
+        //        manageHtmlFormsPage =  homePage.goToAdministration().clickOnManageHtmlForms();
+
+//        if (manageHtmlFormsPage.getElementsIfExisting(By.xpath("//*[contains(text(), '"+ name+ "')]")).isEmpty()) {
+//            manageHtmlFormsPage.clickOnNewHtmlForm();
+//            HtmlFormsPage newHtmlForms = new HtmlFormsPage(manageHtmlFormsPage);
+//            newHtmlForms.CreateNewFormTest(name, description, version);
+//        }
+        homePage.go();
         homePage.goToManageForm();
         if (!manageForm.addPresent()) {
             manageForm.delete();
