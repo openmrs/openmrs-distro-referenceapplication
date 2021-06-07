9eeeabe435238e65a2d87aacb79617a180114eeb
sherrif10 <sharfspace@gmail.com>
2021-06-07 20:31:50 +0300

    RATEST-95:Failing Test: Edit Form Test

--- ui-tests/src/test/java/org/openmrs/reference/EditFormTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/EditFormTest.java
@@ -2,8 +2,9 @@ package org.openmrs.reference;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.openmrs.reference.groups.BuildTests;
 import org.openmrs.reference.page.ClinicianFacingPatientDashboardPage;
 import org.openmrs.reference.page.HeaderPage;
 import org.openmrs.reference.page.HomePage;
@@ -11,7 +12,6 @@ import org.openmrs.reference.page.ManageFormsPage;
 import org.openmrs.uitestframework.test.TestBase;
 
 import static org.junit.Assert.assertNotNull;
-
 
 /**
  * Created by nata on 24.06.15.
@@ -24,21 +24,22 @@ public class EditFormTest extends TestBase {
 
     @Before
     public void setUp() throws Exception {
-
+        
         homePage = new HomePage(page);
-        assertPage(homePage);
+        assertPage(homePage.waitForPage());
         headerPage = new HeaderPage(driver);
         manageForm = new ManageFormsPage(driver);
         patientDashboardPage = new ClinicianFacingPatientDashboardPage(page);
     }
 
-    @Ignore//ignore due to moving forms functionality
     @Test
-    public void EditFormTest() throws Exception {
+    @Category(BuildTests.class)
+    public void editFormTest() throws Exception {
         homePage.goToManageForm();
         if (!manageForm.addPresent()) {
             manageForm.delete();
         }
+
         manageForm.add();
         manageForm.addLabel("Eye Report");
         manageForm.addIcon("icon-align-justify");
@@ -62,8 +63,9 @@ public class EditFormTest extends TestBase {
 
     @After
     public void tearDown() throws Exception {
-        headerPage.clickOnHomeIcon();
-        headerPage.logOut();
+        if (headerPage != null) {
+            headerPage.clickOnHomeIcon();
+            headerPage.logOut();
+        }
     }
 }
-
