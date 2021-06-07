955783893773a7d1b1680154854e420d84958a9c
MAGEMBE SHARIF <48015736+sherrif10@users.noreply.github.com>
2021-06-07 18:58:52 +0300

    RATEST-74: Failing Test: ignored AddFormTests (#403)
    * RATEST-74:Fix AddFormTest
    
    * Resolving failure in override button
    
    * checking gitactions
    
    * recollecting eclipse indetation
    
    * checking failures
    
    * checking failures

--- ui-tests/src/test/java/org/openmrs/reference/page/ManageFormsPage.java
+++ ui-tests/src/test/java/org/openmrs/reference/page/ManageFormsPage.java
@@ -81,7 +81,6 @@ public class ManageFormsPage extends Page {
         clickOn(DELETE_FORM_PATH);
     }
 
-    //    Eye Form
     public void clickOnCalendarEyeReport() {
         clickOn(CALENDAR_EYE_FORM);
     }
@@ -114,11 +113,9 @@ public class ManageFormsPage extends Page {
         clickOn(CURRENT_DAY);
     }
 
-
     @Override
     public String getPageUrl() {
-        return "/formentryapp/forms/extension.page";
+        return "/formentryapp/forms.page";
     }
-
 
 }
