ca516ab6a63f82529c79b77cc64e1c072d6515ed
MAGEMBE SHARIF <48015736+sherrif10@users.noreply.github.com>
2021-06-07 18:58:52 +0300

    RATEST-74: Failing Test: ignored AddFormTests (#403)
    RATEST-93:Failing Test: Delete Form Test
    
    rebuilding the index
    
    rebuildng index
    
    Clean code

--- ui-tests/src/test/java/org/openmrs/reference/page/HeaderPage.java
+++ ui-tests/src/test/java/org/openmrs/reference/page/HeaderPage.java
@@ -3,6 +3,7 @@ package org.openmrs.reference.page;
 import org.openmrs.uitestframework.page.Page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HeaderPage extends Page {
 
@@ -16,14 +17,13 @@ public class HeaderPage extends Page {
     public void clickOnHomeIcon() throws InterruptedException {
         clickOn(By.className(HOME_ICON));
     }
-
-
     // TODO This is unused, do we really need it?
     public void clickOnHomeLink() {
         clickOn(By.linkText(HOME_LINK_TEXT));
     }
 
     public void logOut() throws InterruptedException {
+        waiter.until(ExpectedConditions.visibilityOfElementLocated(LOGOUT));
         clickOn(LOGOUT);
     }
 
