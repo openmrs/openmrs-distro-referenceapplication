210ab1808681012c0150186a8951d8f8364538f5
Kakumirizi Daud <dkakumirizii@gmail.com>
2021-06-30 13:24:00 +0300

    RATEST-145: Fix end visit test (#449)
    * RATEST-145: Fix end visit test
    
    * RATEST-145: Refactoring code base for quality assurance

--- ui-tests/src/test/java/org/openmrs/reference/EndVisitTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/EndVisitTest.java
@@ -11,7 +11,6 @@ package org.openmrs.reference;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.openmrs.reference.groups.BuildTests;
@@ -33,11 +32,11 @@ public class EndVisitTest extends LocationSensitiveApplicationTestBase {
     }
 
     @Test
-    @Ignore
     @Category(BuildTests.class)
-    public void EndVisitTest() throws Exception {
+    public void endVisitTest() {
         FindPatientPage findPatientPage = homePage.goToFindPatientRecord();
         findPatientPage.enterPatient(patient.identifier);
+        findPatientPage.waitForPageToLoad();
         ClinicianFacingPatientDashboardPage clinicianFacingPatientDashboardPage = findPatientPage.clickOnFirstPatient();
         PatientVisitsDashboardPage patientVisitsDashboardPage = clinicianFacingPatientDashboardPage.goToRecentVisits();
         patientVisitsDashboardPage = patientVisitsDashboardPage.endVisit();
@@ -48,5 +47,4 @@ public class EndVisitTest extends LocationSensitiveApplicationTestBase {
     public void tearDown() throws Exception {
         deletePatient(patient);
     }
-
 }
