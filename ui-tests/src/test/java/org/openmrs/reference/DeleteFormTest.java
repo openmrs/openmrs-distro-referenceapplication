79b5074fe04358c4bd101fb0c2e2ff9f43a13145
k-joseph <kaweesi.joseph2012@gmail.com>
2021-07-01 23:23:49 +0300

    Revert "RATEST-93:Failing Test: Delete Form Test (#479)" (#487)
    This reverts commit 067fcbd06937a875999a2560cffca6f47f6da7b1.

--- ui-tests/src/test/java/org/openmrs/reference/DeleteFormTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/DeleteFormTest.java
@@ -2,15 +2,15 @@ package org.openmrs.reference;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.openmrs.reference.groups.BuildTests;
 import org.openmrs.reference.page.HeaderPage;
 import org.openmrs.reference.page.HomePage;
 import org.openmrs.reference.page.ManageFormsPage;
 import org.openmrs.uitestframework.test.TestBase;
 
 import static org.junit.Assert.assertNotNull;
+
 
 /**
  * Created by nata on 24.06.15.
@@ -24,13 +24,13 @@ public class DeleteFormTest extends TestBase {
     public void setUp() throws Exception {
 
         homePage = new HomePage(page);
-        assertPage(homePage.waitForPage());
+        assertPage(homePage);
         headerPage = new HeaderPage(driver);
         manageForm = new ManageFormsPage(driver);
     }
 
+    @Ignore//ignore due to moving forms functionality
     @Test
-    @Category(BuildTests.class)
     public void deleteFormTest() throws Exception {
         homePage.goToManageForm();
         if (!manageForm.addPresent()) {
@@ -50,9 +50,7 @@ public class DeleteFormTest extends TestBase {
 
     @After
     public void tearDown() throws Exception {
-        if (headerPage != null) {
-            headerPage.clickOnHomeIcon();
-            headerPage.logOut();
-        }
+        headerPage.clickOnHomeIcon();
+        headerPage.logOut();
     }
 }
