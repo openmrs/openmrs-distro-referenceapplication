869ac1a07c9950ddfc750a83138a13cdb4f22a06
sherrif10 <sharfspace@gmail.com>
2021-07-14 07:36:55 +0300

    resolving un wanted lines

--- ui-tests/src/test/java/org/openmrs/reference/UnidentifiedPatientKeyboardTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/UnidentifiedPatientKeyboardTest.java
@@ -20,43 +20,43 @@ import org.openmrs.uitestframework.test.TestData;
  * Created by tomasz on 22.07.15.
  */
 public class UnidentifiedPatientKeyboardTest extends TestBase {
-    private HeaderPage headerPage;
-    private RegistrationPage registrationPage;
-    private HomePage homePage;
-    private ClinicianFacingPatientDashboardPage patientDashboardPage;
-    private TestPatient patient;
+	private HeaderPage headerPage;
+	private RegistrationPage registrationPage;
+	private HomePage homePage;
+	private ClinicianFacingPatientDashboardPage patientDashboardPage;
+	private TestPatient patient;
 
-    @Before
-    public void setUp() throws Exception {
-        homePage = new HomePage(page);
-        registrationPage = new RegistrationPage(page);
-        patientDashboardPage = new ClinicianFacingPatientDashboardPage(page);
-        assertPage(homePage.waitForPage());
-    }
+	@Before
+	public void setUp() throws Exception {
+		homePage = new HomePage(page);
+		registrationPage = new RegistrationPage(page);
+		patientDashboardPage = new ClinicianFacingPatientDashboardPage(page);
+		assertPage(homePage.waitForPage());
+	}
 
-    @After
-    public void tearDown() throws Exception {
-        TestData.PatientInfo p = new TestData.PatientInfo();
-        p.uuid = patient.uuid;
-        deletePatient(p);
-        waitForPatientDeletion(patient.uuid);
-    }
+	@After
+	public void tearDown() throws Exception {
+		TestData.PatientInfo p = new TestData.PatientInfo();
+		p.uuid = patient.uuid;
+		deletePatient(p);
+		waitForPatientDeletion(patient.uuid);
+	}
 
-    @Test
-    @Category(BuildTests.class)
-    public void unidentifiedPatientKeyboardTest() throws InterruptedException {
-        homePage.goToRegisterPatientApp();
-        patient = PatientGenerator.generateTestPatient();
-        registrationPage.enterUnidentifiedPatient(patient);
+	@Test
+	@Category(BuildTests.class)
+	public void unidentifiedPatientKeyboardTest() throws InterruptedException {
+		homePage.goToRegisterPatientApp();
+		patient = PatientGenerator.generateTestPatient();
+		registrationPage.enterUnidentifiedPatient(patient);
 
-        assertTrue(registrationPage.getNameInConfirmationPage().contains("--"));
-        assertTrue(registrationPage.getGenderInConfirmationPage().contains(patient.gender));
+		assertTrue(registrationPage.getNameInConfirmationPage().contains("--"));
+		assertTrue(registrationPage.getGenderInConfirmationPage().contains(patient.gender));
 
-        patientDashboardPage = registrationPage.confirmPatient();
-        patientDashboardPage.waitForPage();
+		patientDashboardPage = registrationPage.confirmPatient();
+		patientDashboardPage.waitForPage();
 
-        patient.uuid = patientDashboardPage.getPatientUuidFromUrl();
-        assertPage(patientDashboardPage.waitForPage()); // remember just-registered patient id, so it can be removed.
-        assertTrue(driver.getPageSource().contains("UNKNOWN UNKNOWN"));
-    }
+		patient.uuid = patientDashboardPage.getPatientUuidFromUrl();
+		assertPage(patientDashboardPage.waitForPage()); // remember just-registered patient id, so it can be removed.
+		assertTrue(driver.getPageSource().contains("UNKNOWN UNKNOWN"));
+	}
 }
--- ui-tests/src/test/java/org/openmrs/reference/UsingBackButtonInMergePatientTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/UsingBackButtonInMergePatientTest.java
@@ -21,67 +21,67 @@ import static org.junit.Assert.assertNotNull;
  */
 
 public class UsingBackButtonInMergePatientTest extends TestBase {
-    private HomePage homePage;
-    private HeaderPage headerPage;
-    private TestPatient patient;
-    private TestPatient patient1;
-    private RegistrationPage registrationPage;
-    private ClinicianFacingPatientDashboardPage patientDashboardPage;
-    private DataManagementPage dataManagementPage;
-    private String id;
-    private String id2;
+	private HomePage homePage;
+	private HeaderPage headerPage;
+	private TestPatient patient;
+	private TestPatient patient1;
+	private RegistrationPage registrationPage;
+	private ClinicianFacingPatientDashboardPage patientDashboardPage;
+	private DataManagementPage dataManagementPage;
+	private String id;
+	private String id2;
 
-    @Before
-    public void setUp() throws Exception {
-        homePage = new HomePage(page);
-        assertPage(homePage.waitForPage());
-        headerPage = new HeaderPage(driver);
-        registrationPage = new RegistrationPage(page);
-        patientDashboardPage = new ClinicianFacingPatientDashboardPage(page);
-        dataManagementPage = new DataManagementPage(page);
-        patient = new TestPatient();
-        patient1 = new TestPatient();
-    }
+	@Before
+	public void setUp() throws Exception {
+		homePage = new HomePage(page);
+		assertPage(homePage.waitForPage());
+		headerPage = new HeaderPage(driver);
+		registrationPage = new RegistrationPage(page);
+		patientDashboardPage = new ClinicianFacingPatientDashboardPage(page);
+		dataManagementPage = new DataManagementPage(page);
+		patient = new TestPatient();
+		patient1 = new TestPatient();
+	}
 
-    @Test
-    @Category(BuildTests.class)
-    public void usingBackButtonInMergePatientTest() throws Exception {
-        homePage.goToRegisterPatientApp().waitForPage();
-        patient.familyName = "Potter";
-        patient.givenName = "John";
-        patient.gender = "Male";
-        patient.estimatedYears = "45";
-        patient.address1 = "address";
-        registrationPage.enterMergePatient(patient);
-        id = patientDashboardPage.findPatientId();
-        patient.uuid =  patientDashboardPage.getPatientUuidFromUrl();
-        headerPage.clickOnHomeIcon();
-        homePage.goToRegisterPatientApp();
-        patient1.familyName = "Smith";
-        patient1.givenName = "Jane";
-        patient1.gender = "Female";
-        patient1.estimatedYears = "25";
-        patient1.address1 = "address";
-        registrationPage.enterMergePatient(patient1);
-        id2 = patientDashboardPage.findPatientId();
-        headerPage.clickOnHomeIcon();
-        homePage.goToDataManagement();
-        dataManagementPage.goToMergePatient();
-        dataManagementPage.enterPatient1(id);
-        dataManagementPage.enterPatient2(id2);
-        dataManagementPage.searchId(id);
-        dataManagementPage.clickOnContinue();
-        dataManagementPage.clickOnNo();
-        dataManagementPage.enterPatient1(id);
-        assertNotNull(dataManagementPage.CONTINUE);
-    }
+	@Test
+	@Category(BuildTests.class)
+	public void usingBackButtonInMergePatientTest() throws Exception {
+		homePage.goToRegisterPatientApp().waitForPage();
+		patient.familyName = "Potter";
+		patient.givenName = "John";
+		patient.gender = "Male";
+		patient.estimatedYears = "45";
+		patient.address1 = "address";
+		registrationPage.enterMergePatient(patient);
+		id = patientDashboardPage.findPatientId();
+		patient.uuid = patientDashboardPage.getPatientUuidFromUrl();
+		headerPage.clickOnHomeIcon();
+		homePage.goToRegisterPatientApp();
+		patient1.familyName = "Smith";
+		patient1.givenName = "Jane";
+		patient1.gender = "Female";
+		patient1.estimatedYears = "25";
+		patient1.address1 = "address";
+		registrationPage.enterMergePatient(patient1);
+		id2 = patientDashboardPage.findPatientId();
+		headerPage.clickOnHomeIcon();
+		homePage.goToDataManagement();
+		dataManagementPage.goToMergePatient();
+		dataManagementPage.enterPatient1(id);
+		dataManagementPage.enterPatient2(id2);
+		dataManagementPage.searchId(id);
+		dataManagementPage.clickOnContinue();
+		dataManagementPage.clickOnNo();
+		dataManagementPage.enterPatient1(id);
+		assertNotNull(dataManagementPage.CONTINUE);
+	}
 
-    @After
-    public void tearDown() throws Exception {
-        headerPage.clickOnHomeIcon();
-        TestData.PatientInfo p = new TestData.PatientInfo();
-        p.uuid = patient.uuid;
-        deletePatient(p);
-        waitForPatientDeletion(patient.uuid);
-    }
+	@After
+	public void tearDown() throws Exception {
+		headerPage.clickOnHomeIcon();
+		TestData.PatientInfo p = new TestData.PatientInfo();
+		p.uuid = patient.uuid;
+		deletePatient(p);
+		waitForPatientDeletion(patient.uuid);
+	}
 }
