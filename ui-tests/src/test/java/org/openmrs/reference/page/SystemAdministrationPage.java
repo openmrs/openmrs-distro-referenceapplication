51959c809aded03e0154a01319aa934c3dcd2919
kdaud <dkakumirizii@gmail.com>
2021-08-02 19:57:05 +0300

    RATEST-157: Adding functionality for manage user test workflow

--- ui-tests/src/test/java/org/openmrs/reference/page/SystemAdministrationPage.java
+++ ui-tests/src/test/java/org/openmrs/reference/page/SystemAdministrationPage.java
@@ -17,6 +17,7 @@ public class SystemAdministrationPage extends Page {
 
     private static final By ADVANCED_ADMINISTRATION = By.id("referenceapplication-legacyAdmin-app");
     private static By STYLES_GUIDE_LINK = By.id("referenceapplication-styleGuide-app");
+    private static By MANAGE_ACCOUNTS = By.cssSelector("#org-openmrs-module-adminui-accounts-app i");
 
     public SystemAdministrationPage(Page parent) {
         super(parent);
@@ -32,6 +33,11 @@ public class SystemAdministrationPage extends Page {
         return new AdministrationPage(this);
     }
 
+    public ManageUserAccountPage goToManageAccounts() {
+        clickOn(MANAGE_ACCOUNTS);
+        return new ManageUserAccountPage(this);
+    }
+    
     public StylesGuidePage clickOnStylesGuideAppLink() {
         clickOn(STYLES_GUIDE_LINK);
         return new StylesGuidePage(this);
