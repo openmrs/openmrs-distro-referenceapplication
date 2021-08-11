935b282891c71324321c8feca7a57db892af82d4
insookwa <nsookwaivan@gmail.com>
2021-08-11 14:58:44 +0300

    RA-1888:Mark patient deseased test

---
+++ ui-tests/src/test/java/org/openmrs/reference/MarkPatientDeceasedTest.java
@@ -0,0 +1,52 @@
+package org.openmrs.reference;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.openmrs.reference.groups.BuildTests;
+import org.openmrs.reference.page.FindPatientPage;
+import org.openmrs.reference.page.MarkPatientDeceasedPage;
+import org.openmrs.uitestframework.test.TestData;
+
+public class MarkPatientDeceasedTest extends ReferenceApplicationTestBase {
+	
+	private TestData.PatientInfo patient;
+	
+	@Before
+	public void setUp() throws Exception {
+		patient = createTestPatient();
+		createTestVisit();
+		
+	}
+	
+	@Test
+	@Category(BuildTests.class)
+	public void markPatientDeceasedTest() {
+		String HOME_LINK = "login.htm";
+		homePage.goToAdministration();
+		MarkPatientDeceasedPage markPatientDeceased = new MarkPatientDeceasedPage(page);
+		markPatientDeceased.getConcept();
+		FindPatientPage findPatientPage = homePage.goToFindPatientRecord();
+		findPatientPage.enterPatient(patient.identifier);
+		findPatientPage.waitForPageToLoad();
+		findPatientPage.clickOnFirstPatient();
+		markPatientDeceased.markPatientDead();
+		assertTrue(markPatientDeceased.confirmDeadMessage().contains("The patient is deceased"));
+		page.goToPage(HOME_LINK);
+		homePage.goToAdministration();
+		markPatientDeceased.deleteConcept();
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		deletePatient(patient);
+	}
+	
+	private void createTestVisit() {
+		new TestData.TestVisit(patient.uuid, TestData.getAVisitType(), getLocationUuid(homePage)).create();
+	}
+}
