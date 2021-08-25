66f10bc7538a282a0f8943866b2a18847d09a76e
sherrif10 <sharfspace@gmail.com>
2021-08-25 12:37:32 +0300

    RATEST-117:RATEST-117:EditPatientRelationshipPage

---
+++ ui-tests/src/test/java/org/openmrs/reference/page/RegistrationSummaryPage.java
@@ -0,0 +1,25 @@
+package org.openmrs.reference.page;
+
+import org.openmrs.uitestframework.page.Page;
+import org.openqa.selenium.By;
+
+public class RegistrationSummaryPage extends Page {
+
+    private static final By RELATIONSHIP = By.id("relationships-info-edit-link");
+    private static final By DEMOGRAPHICS = By.id("demographics-edit-link");
+    private  static final By CONTACT_INFO = By.id("contactInfo-edit-link");
+
+    public RegistrationSummaryPage(Page parent) {
+        super(parent);
+    }
+
+    public EditPatientRelationshipPage goToEditPatientRelationship(){
+        clickOnLast(RELATIONSHIP);
+        return new EditPatientRelationshipPage(this);
+    }
+
+    @Override
+    public String getPageUrl() {
+        return "/registrationapp/registrationSummary.page";
+    }
+}
