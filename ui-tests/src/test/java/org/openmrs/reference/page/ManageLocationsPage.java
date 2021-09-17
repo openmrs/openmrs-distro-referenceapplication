d35912f34c5e984a30d06b20d50ab69117cf29e6
Kakumirizi Daud <dkakumirizii@gmail.com>
2021-09-17 16:52:33 +0300

    RATEST-200: Adding page functionality for location workflow test (#535)
    * RATEST-200: Adding page functionality for e2e location workflow test
    
    * RATEST-200: Refactor logic for retireLocation() function
    
    * RATEST-200: Rename methods and refactoring page element names
    
    * RATEST-200: Renaming variables to clear readable names
    
    * RATEST-200: Refactor the logic of selectParentLocation() within manageLocation page
    
    * RATEST-200: Rename selectParentLocation() to setParentLocation() within manageLocations page
    
    * RATEST-200: Refactoring setParentLocation() to ensure parent location is set
    
    * RATEST-200: Use className() instead of cssSelector() to retrieve parent location field
    
    * RATEST-200: Use cssSelecter() instead of xPath() in retrieving addNewLocationButton on the page
    
    * RATEST-200: Replace the element reference value of CITY_FILED with #testing123

--- ui-tests/src/test/java/org/openmrs/reference/page/ManageLocationsPage.java
+++ ui-tests/src/test/java/org/openmrs/reference/page/ManageLocationsPage.java
@@ -15,10 +15,24 @@ import org.openqa.selenium.TimeoutException;
 
 public class ManageLocationsPage extends Page {
 
-    public static final By CONFIRM_PURGE_BUTTON = By.cssSelector("#adminui-purge-location-dialog > div.dialog-content > form > button.confirm.right");
+    protected static final String PAGE_URL = "/adminui/metadata/locations/manageLocations.page";
+    private static final String PARENT_LOCATION = "Pharmacy";
+    private static final By EDIT_LOCATION_LINK = By.cssSelector("i.icon-pencil.edit-action");
+    public static final By CONFIRM_PURGE_BUTTON = By.cssSelector("#adminui-purge-location-dialog button.confirm.right");
     public static final By CONFIRM_RETIRE_BUTTON = By.cssSelector("#adminui-retire-location-dialog > div.dialog-content > form > button.confirm.right");
-    protected static final String PAGE_URL = "adminui/metadata/locations/manageLocations.page";
+    private static final By LOCATION_TAG_BUTTON = By.cssSelector("#locationForm fieldset table tbody tr:first-child td:first-child input[type=\"checkbox\"]");
+    private static final By ADD_NEW_LOCATION_BUTTON = By.cssSelector("a.button");
+    private static final By LOCATION_NAME_FIELD = By.id("name-field");  
     private static final By ADD_EDIT_LOCATION = By.cssSelector("#content a[href='/openmrs/" + AddEditLocationPage.PAGE_URL + "']");
+    private static final By LOCATION_DESCRIPTION_FIELD = By.cssSelector("#description-field");
+    private static final By LOCATION_ADDRESS_FIELD_1 = By.cssSelector("#address1-field");
+    private static final By LOCATION_ADDRESS_FIELD_2 = By.cssSelector("#address2-field");
+    private static final By CITY_FIELD = By.cssSelector("#testing123");
+    private static final By STATE_FIELD = By.cssSelector("#stateProvince-field");
+    private static final By COUNTRY_FIELD = By.cssSelector("#country-field");
+    private static final By POSTAL_CODE_FIELD = By.cssSelector("#postalCode-field");
+    private static final By PARENT_LOCATION_FIELD = By.className("parentLocation");
+    private static final By SAVE_BUTTON = By.id("save-button");
     private static final String PURGE_LOCATION_SELECTOR_TMPL = "list-locations > tbody > tr > i[onclick*='purgeLocation('%s', *)']";
 
     public ManageLocationsPage(Page parent) {
@@ -35,23 +49,83 @@ public class ManageLocationsPage extends Page {
         return new AddEditLocationPage(this);
     }
 
-    public void purgeLocation(String name) {
-        clickOn(By.xpath("//tr/td[preceding-sibling::td[contains(text(), '" + name + "')]]/i[@class='icon-trash delete-action']"));
+    public void goToAddNewLocation() {
+        clickOn(ADD_NEW_LOCATION_BUTTON);
+    }
+
+    public void goToEditLocationForm() {
+    	clickOn(EDIT_LOCATION_LINK);
+    }
+    
+    public void enterLocationName(String locationName) {     
+        findElement(LOCATION_NAME_FIELD).clear();
+        findElement(LOCATION_NAME_FIELD).sendKeys(locationName);
+    }
+
+    public void enterLocationDescription(String locationDescription) {
+        findElement(LOCATION_DESCRIPTION_FIELD).clear();
+        findElement(LOCATION_DESCRIPTION_FIELD).sendKeys(locationDescription);
+    }
+
+    public void enterAddress1(String address) {
+        findElement(LOCATION_ADDRESS_FIELD_1).clear();
+        findElement(LOCATION_ADDRESS_FIELD_1).sendKeys(address);
+    }
+    
+    public void enterAddress2(String address) {
+        findElement(LOCATION_ADDRESS_FIELD_2).clear();
+        findElement(LOCATION_ADDRESS_FIELD_2).sendKeys(address);
+    }
+    
+    public void enterState(String stateName) {
+        findElement(STATE_FIELD).clear();
+        findElement(STATE_FIELD).sendKeys(stateName);
+    }
+
+    public void enterCity(String cityName) {
+        findElement(CITY_FIELD).clear();
+        findElement(CITY_FIELD).sendKeys(cityName);
+    }
+    
+    public void enterCountry(String countryName) {
+        findElement(COUNTRY_FIELD).clear();
+        findElement(COUNTRY_FIELD).sendKeys(countryName);
+    }
+    
+    public void enterPostalCode(String postalCode) {
+        findElement(POSTAL_CODE_FIELD).clear();
+        findElement(POSTAL_CODE_FIELD).sendKeys(postalCode);
+    }
+    
+    public void setParentLocation() {
+        selectFrom(PARENT_LOCATION_FIELD, PARENT_LOCATION);      	   
+    }
+
+    public void selectLocationTag() {
+        clickOn(LOCATION_TAG_BUTTON);
+    }
+
+    public void saveLocation() {
+        clickOn(SAVE_BUTTON);
+    }
+    
+    public void purgeLocation(String locationName) {
+        clickOn(By.xpath("//tr/td[preceding-sibling::td[contains(text(), '" + locationName + "')]]/i[@class='icon-trash delete-action']"));
         waitForElement(CONFIRM_PURGE_BUTTON);
         clickOn(CONFIRM_PURGE_BUTTON);
     }
 
-    public void retireLocation(String name) {
-        clickOn(By.xpath("//tr/td[preceding-sibling::td[contains(text(), '" + name + "')]]/i[@class='icon-remove delete-action']"));
+    public void retireLocation(String locationName) {
+        clickOn(By.xpath("//tr/td[preceding-sibling::td[contains(text(), '" + locationName + "')]]/i[@class='icon-remove delete-action']"));
         waitForElement(CONFIRM_RETIRE_BUTTON);
         clickOn(CONFIRM_RETIRE_BUTTON);
     }
 
-    public void assertRetired(String name) {
+    public void assertRetired(String locationName) {
         try {
-            findElement(By.xpath("//tr/td[preceding-sibling::td[contains(text(), '" + name + "')]]/i[@class='icon-reply edit-action']"));
+            findElement(By.xpath("//tr/td[preceding-sibling::td[contains(text(), '" + locationName + "')]]/i[@class='icon-reply edit-action']"));
         } catch (TimeoutException e) {
-            throw new RuntimeException("Couldn't find restore button, failed to retire location " + name);
+            throw new RuntimeException("Couldn't find restore button, failed to retire location " + locationName);
         }
     }
 }
