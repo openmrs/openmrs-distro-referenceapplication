c80fa89a6db73426e76e4752d26ae92d3dc29fec
Muta-Jonathan <mutajonathan4@gmail.com>
2021-09-23 11:17:48 +0300

    RATEST-235: Handled Exceptions in the catch block Method ~ UI-TESTS

--- ui-tests/src/test/java/org/openmrs/reference/page/PatientCaptureVitalsPage.java
+++ ui-tests/src/test/java/org/openmrs/reference/page/PatientCaptureVitalsPage.java
@@ -37,11 +37,9 @@ public class PatientCaptureVitalsPage extends Page {
     
     public void setHeightField(String value) {
         WebElement heightField = findElement(HEIGHT_FIELD);
-        try {
             heightField.clear();
             heightField.sendKeys(value);
             findElement(By.xpath("//ul[@id='formBreadcrumb']/li/ul/li[2]")).click();
-        } catch (Exception e) {}
     }
 
     public void clearPatientWeight() {
@@ -50,11 +48,9 @@ public class PatientCaptureVitalsPage extends Page {
     
     public void setWeightField(String value) {
         WebElement weightField = findElement(WEIGHT_FIELD);
-        try {
             weightField.clear();
             weightField.sendKeys(value);
             findElement(By.xpath("//ul[@id='formBreadcrumb']/li/ul/li[4]")).click();
-        } catch (Exception e) {}
     }
 
     public void clearPatientTemperature() {
@@ -63,11 +59,9 @@ public class PatientCaptureVitalsPage extends Page {
     
     public void setTemperatureField(String value) {
         WebElement temperatureField = findElement(TEMPERATURE_FIELD);
-        try {
             temperatureField.clear();
             temperatureField.sendKeys(value);
             findElement(By.xpath("//ul[@id='formBreadcrumb']/li/ul/li[5]")).click();
-        } catch (Exception e) {}
     }
 
     public void clearPatientPulse() {
@@ -76,11 +70,9 @@ public class PatientCaptureVitalsPage extends Page {
     
     public void setPulseField(String value) {
         WebElement pulseField = findElement(PULSE_FIELD);
-        try {
             pulseField.clear();
             pulseField.sendKeys(value);
             findElement(By.xpath("//ul[@id='formBreadcrumb']/li/ul/li[6]")).click();
-        } catch (Exception e) {}
     }
 
     public void clearPatientRespiratoryRate() {
@@ -89,11 +81,9 @@ public class PatientCaptureVitalsPage extends Page {
     
     public void setRespiratoryField(String value) {
         WebElement respiratoryField = findElement(RESPIRATORY_FIELD);
-        try {
             respiratoryField.clear();
             respiratoryField.sendKeys(value);
             findElement(By.xpath("//ul[@id='formBreadcrumb']/li/ul/li[7]")).click();
-        } catch (Exception e) {}
     }
 
     public void clearPatientBloodPressure1() {
@@ -106,17 +96,13 @@ public class PatientCaptureVitalsPage extends Page {
     
     public void setBloodPressureFields(String value1, String value2) {
         WebElement bloodPressureField1 = findElement(BLOOD_PRESSURE_FIELD_1);
-        try {
             bloodPressureField1.clear();
             bloodPressureField1.sendKeys(value1);
-        } catch (Exception e) {}
 
         WebElement bloodPressureField2 = findElement(BLOOD_PRESSURE_FIELD_2);
-        try {
             bloodPressureField2.clear();
             bloodPressureField2.sendKeys(value2);
             findElement(By.xpath("//ul[@id='formBreadcrumb']/li/ul/li[8]")).click();
-        } catch (Exception e) {}
     }
 
     public void clearPatientBloodOxygenSaturation() {
@@ -125,18 +111,14 @@ public class PatientCaptureVitalsPage extends Page {
     
     public void setBloodOxygenSaturationField(String value) {
         WebElement bloodOxygenSaturationField = findElement(BLOOD_OXYGEN_SATURATION_FIELD);
-        try {
             bloodOxygenSaturationField.clear();
             bloodOxygenSaturationField.sendKeys(value);
             findElement(By.xpath("//ul[@id='formBreadcrumb']/li/ul/li[2]")).click();
-        } catch (Exception e) {}
     }
 
     public void confirm() {
-        try {
             WebElement confirmButton = findElement(CONFIRM_BUTTON);
             confirmButton.click();
-        } catch (Exception e) {}
     }
 
     public boolean save() {
