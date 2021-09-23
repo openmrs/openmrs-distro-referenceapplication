fd4e873d3d84aefcf918e583ee593da4c9310d36
Ian <ian_bacher@brown.edu>
2021-09-23 15:39:40 -0400

    Add support for overriding the importmap URL via an environment variable

--- run/docker/ui/startup.sh
+++ run/docker/ui/startup.sh
@@ -4,6 +4,11 @@ set -e
 # Apache gets grumpy about PID files pre-existing
 rm -f /usr/local/apache2/logs/httpd.pid
 
+if [ -n "${IMPORTMAP_URL}" ]; then
+  # substitute importmap.json for $IMPORTMAP_URL allowimg us to populate it from a URL
+  sed -i -e 's/"importmap.json"/"$IMPORTMAP_URL"/g' "/var/www/ui/index.html"
+fi
+
 # Substitute envvars in packaged files
 envsubst < "/var/www/ui/index.html" | sponge "/var/www/ui/index.html"
 
