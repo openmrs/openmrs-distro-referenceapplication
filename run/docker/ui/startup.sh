b048695f115ebdfb9ed34499c1531c25701f32e7
Ian <ian_bacher@brown.edu>
2021-09-26 14:19:53 -0400

    Improve the handling of SPA_CONFIG_URLS to allow list of URLs

--- run/docker/ui/startup.sh
+++ run/docker/ui/startup.sh
@@ -7,6 +7,32 @@ rm -f /usr/local/apache2/logs/httpd.pid
 if [ -n "${IMPORTMAP_URL}" ]; then
   # substitute importmap.json for $IMPORTMAP_URL allowimg us to populate it from a URL
   sed -i -e 's/"importmap.json"/"$IMPORTMAP_URL"/g' "/var/www/ui/index.html"
+
+# setting the config urls to "" causes an error reported in the console, so if we aren't using
+# the SPA_CONFIG_URLS, we'll leave it alone
+if [ -z "$SPA_CONFIG_URLS" ]; then
+  sed -i -e 's/"$SPA_CONFIG_URLS"//' "/usr/share/nginx/html/index.html"
+# otherwise convert the URLs into a Javascript list
+# we support two formats, a comma-separated list or a space separated list
+else
+  old_IFS="$IFS"
+  if echo "$SPA_CONFIG_URLS" | grep , >/dev/null; then
+    IFS=","
+  fi
+
+  CONFIG_URLS=
+  for url in $SPA_CONFIG_URLS;
+  do
+    if [ -z "$CONFIG_URLS" ]; then
+      CONFIG_URLS="\"${url}\""
+    else
+      CONFIG_URLS="$CONFIG_URLS,\"${url}\""
+    fi
+  done
+
+  IFS="$old_IFS"
+  export SPA_CONFIG_URLS=CONFIG_URLS
+  sed -i -e 's/"$SPA_CONFIG_URLS"/$SPA_CONFIG_URLS/' "/usr/share/nginx/html/index.html"
 fi
 
 # Substitute envvars in packaged files
