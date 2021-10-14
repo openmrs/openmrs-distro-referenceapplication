4865c9a8974f455ab144e497c32e1282e89346da
sherrif10 <sharfspace@gmail.com>
2021-10-14 12:57:43 +0300

    RATEST-224:Adding Pages for report difinition workflow

---
+++ ui-tests/src/test/java/org/openmrs/reference/page/EditReportPage.java
@@ -0,0 +1,44 @@
+package org.openmrs.reference.page;
+
+import org.openmrs.uitestframework.page.Page;
+import org.openqa.selenium.By;
+
+public class EditReportPage extends Page {
+
+   private static final By PREVIEW_BUTTON = By.cssSelector("#previewButton");
+   private static final By START_DATE_FIELD = By.cssSelector("#startDate");
+   private static final By END_DATE_FIELD = By.cssSelector("#endDate");
+   private static final By RUN_BUTTON = By.cssSelector("#preview-parameterizable-button");
+   private static final By CLOSE_TAB = By.xpath("//span[contains(text(),'close')]");
+   private static final String startDate = "05/07/2008";
+   private static final String endDate = "20/06/2020";
+
+   public EditReportPage(Page parent) {
+      super(parent);
+   }
+
+   public void clickOnPreviewButton() {
+      clickOn(PREVIEW_BUTTON);
+   }
+
+   public void enterStartDate(String startDate) {
+      setTextToFieldNoEnter(START_DATE_FIELD, startDate);
+   }
+ 
+   public void enterEndDate(String startDate) {
+       setTextToFieldNoEnter(END_DATE_FIELD, startDate);
+   }
+ 
+   public void clickOnRun() {
+      clickOn(RUN_BUTTON);
+   }
+ 
+   public void clickOnCloseTab() {
+      clickOn(CLOSE_TAB);
+   }
+ 
+   @Override
+   public String getPageUrl() {
+      return "/reporting/reports/reportEditor.form?uuid=b39c4c4c-4881-11e7-a919-92ebcb67fe33";
+   }
+}
