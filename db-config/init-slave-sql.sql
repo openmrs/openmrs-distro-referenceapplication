6fbf657b9db3b06acc61d58cbc804b37a29338bb
MarceloGal <marcelogal619@gmail.com>
2025-01-15 18:15:46 -0500

    test sql

--- db-config/init-slave-sql.sql
+++ db-config/init-slave-sql.sql
@@ -1,7 +1,6 @@
 CHANGE MASTER TO 
-  MASTER_HOST='mariadb_primary',
+  MASTER_HOST='db',
   MASTER_USER='openmrs_repl',
   MASTER_PASSWORD='openmrs_repl',
-  MASTER_AUTO_POSITION=1;
-  
+  MASTER_AUTO_POSITION=1;  
 START SLAVE;
