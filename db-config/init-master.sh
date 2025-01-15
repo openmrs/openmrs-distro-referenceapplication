48d184f7cbc17b772575d3f0b0e138fd1eb501f9
MarceloGal <marcelogal619@gmail.com>
2025-01-15 17:11:26 -0500

    edited mysql root passwords

--- db-config/init-master.sh
+++ db-config/init-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-mysql -uroot -p "${MYSQL_ROOT_PASSWORD:-openmrs}" <<-EOSQL
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD:-openmrs}" <<-EOSQL
   CREATE USER 'openmrs_repl'@'%' IDENTIFIED BY 'openmrs_repl';
   GRANT REPLICATION SLAVE ON *.* TO 'openmrs_repl'@'%';
   FLUSH PRIVILEGES;
