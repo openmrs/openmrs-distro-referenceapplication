7b9a608dee259faff4b133f4749a7fb89e79babd
MarceloGal <marcelogal619@gmail.com>
2025-01-15 21:45:08 -0500

    clean up and some adjustments

--- db-config/init-master.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-mysql -uroot -p"${MYSQL_ROOT_PASSWORD:-openmrs}" <<-EOSQL
-  CREATE USER 'openmrs_repl'@'%' IDENTIFIED BY 'openmrs_repl';
-  GRANT REPLICATION SLAVE ON *.* TO 'openmrs_repl'@'%';
-  FLUSH PRIVILEGES;
-EOSQL
