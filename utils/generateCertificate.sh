f08f93a1d720c9a9ed95333d880390bc5cc36dda
MarceloGal <marcelogal619@gmail.com>
2025-02-20 10:25:17 -0500

    minor fix to certificate script

--- utils/generateCertificate.sh
+++ utils/generateCertificate.sh
@@ -10,18 +10,18 @@ SERGVER_IP=192.168.1.xxxx
 RUN apt-get update && apt-get install -y openssl
 
 # Creating directory to save certificate
-RUN mkdir -p ./ssl
+RUN mkdir -p ssl
 
 # Generate private key
-echo "Generating private key and crt with prefix: $CERT_NAME ..."
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./ssl/$CERT_NAME.key -out ./ssl/$CERT_NAME.crt -subj "/C=PE/ST=Loreto/L=Maynas/O=Centro de Salud Santa Clotilde/OU=peruHCE/CN=peruHCE-server" -addext "subjectAltName = DNS:localhost, IP:127.0.0.1, IP:192.168.200.240"  # Replace with your LAN IP
+echo "Generating private key and crt with prefix: ${CERT_NAME} ..."
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./ssl/${CERT_NAME}.key -out ./ssl/${CERT_NAME}.crt -subj "/C=PE/ST=Loreto/L=Maynas/O=Centro de Salud Santa Clotilde/OU=peruHCE/CN=peruHCE-server" -addext "subjectAltName = DNS:localhost, IP:127.0.0.1, IP:192.168.200.240"  # Replace with your LAN IP
 
 echo "Certificate generated successfully:"
 
 # Display certificate details
 echo -e "\n🔍 Certificate Details:"
-openssl x509 -in "./ssl/$CERT_NAME.crt" -text -noout
+openssl x509 -in "./ssl/${CERT_NAME}.crt" -text -noout
 
 # Display private key details
 echo -e "\n🔑 Private Key Details:"
-openssl rsa -in "./ssl/$CERT_NAME.key" -text -noout
+openssl rsa -in "./ssl/${CERT_NAME}.key" -text -noout
