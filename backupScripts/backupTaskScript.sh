08e6a21bfb67209d74f7e36b93841176d9c6f72e
MarceloGal <marcelogal619@gmail.com>
2025-02-26 16:31:28 -0500

    added to clean the /backup dir in the replica container

--- backupScripts/backupTaskScript.sh
+++ backupScripts/backupTaskScript.sh
@@ -10,22 +10,25 @@ TEMP_BACKUP_PATH="/backup"              # Inside the container
 
 echo "Starting MariaDB replic backup..."
 
-# 1️⃣ Run physical backup inside the MariaDB container
+# Clean the backup directorie in the replica container
+docker exec --user root $CONTAINER_NAME rm -rf "$TEMP_BACKUP_PATH"
+
+# Run physical backup inside the MariaDB container
 docker exec --user root $CONTAINER_NAME mariadb-backup --user=root --password=${OMRS_DB_R_PASSWORD:-openmrs_r} --backup --target-dir=$TEMP_BACKUP_PATH
 
-# 2️⃣ Copy the backup to the host
+# Copy the backup to the host
 docker cp "$CONTAINER_NAME:$TEMP_BACKUP_PATH" "$BACKUP_DIR/$BACKUP_NAME"
 
-# 3️⃣ Zip the backup
+# Zip the backup
 cd "$BACKUP_DIR" || exit
 tar -czf "$BACKUP_NAME.tar.gz" "$BACKUP_NAME"
 
-# 4️⃣ Remove the uncompressed backup directory
+# Remove the uncompressed backup directory
 rm -rf "$BACKUP_NAME"
 
 echo "Backup created: $BACKUP_DIR/$BACKUP_NAME.tar.gz"
 
-# 5️⃣ Rotate old backups: Keep only the latest $MAX_BACKUPS backups
+# Rotate old backups: Keep only the latest $MAX_BACKUPS backups
 BACKUP_COUNT=$(ls -1 "$BACKUP_DIR"/*.tar.gz 2>/dev/null | wc -l)
 
 if [ "$BACKUP_COUNT" -gt "$MAX_BACKUPS" ]; then
