64bd9da2c52284e8c397380263295da131db34ea
MarceloGal <marcelogal619@gmail.com>
2025-03-06 10:35:06 -0500

    erased env file temporaly

--- utils/fullInit.sh
+++ utils/fullInit.sh
@@ -24,7 +24,7 @@ fi
 
 # Build and start the Docker Compose stack
 echo "Building and starting the Docker Compose stack..."
-sudo docker compose up -d --build --env-file enviromentVariables.env
+sudo docker compose up -d --build 
 
 # Check if the stack started successfully
 if [ $? -eq 0 ]; then
