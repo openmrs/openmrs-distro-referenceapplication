a23d5a1d7a2663f6fafd5f380ea1ac69b39f8310
Maximilian Irlinger <max@maxirlinger.at>
2025-12-12 01:15:30 +0100

    add Augen Auf OpenMRS frontend

--- cleanup.sh
+++ cleanup.sh
@@ -2,5 +2,5 @@
 
 podman rm openmrs-distro-referenceapplication-frontend-1 openmrs-distro-referenceapplication-gateway-1 openmrs-distro-referenceapplication-db-1 openmrs-distro-referenceapplication-backend-1
 podman volume rm openmrs-distro-referenceapplication_db-data openmrs-distro-referenceapplication_openmrs-data
-podman rmi docker.io/library/openmrs-distro-referenceapplication-backend
+podman rmi docker.io/library/openmrs-distro-referenceapplication-backend docker.io/library/openmrs-distro-referenceapplication-frontend
 
