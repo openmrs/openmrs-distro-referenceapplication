a16633f2f8d6fc2d20ed6063d05322f57a799c16
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2026-04-20 11:39:51 -0500

    STANDALONE FRONTEND

--- frontend/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Copy brand assets into the shared SPA volume (populated by frontend-init).
-# We use cp -n so we never overwrite files the init container placed there.
-cp -n /brand/* /usr/share/nginx/html/ 2>/dev/null || true
-
-exec nginx -g "daemon off;"
